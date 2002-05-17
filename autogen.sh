a19cc1f83fb4ad89b68bcf46b6a738a9176148fe
Nalin Dahyabhai <nalin@src.gnome.org>
2002-05-17 02:25:25 +0000

    Try multiple versions of autoconf, too.
    	* autogen.sh: Try multiple versions of autoconf, too.

--- autogen.sh
+++ autogen.sh
@@ -2,10 +2,10 @@
 CFLAGS="${CFLAGS:--g3 -O -Wall}" ; export CFLAGS
 set -x
 libtoolize -f -c
-autoheader
+autoheader-2.13 || autoheader
 aclocal-1.5 || aclocal
 automake-1.5 -a || automake -a
-autoconf
+autoconf-2.13 || autoconf
 if test -f config.cache ; then
 	rm -f config.cache
 fi
