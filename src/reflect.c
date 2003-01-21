83c4f654fefd5132acb8e28af3bb4730bb23d4da
Nalin Dahyabhai <nalin@src.gnome.org>
2003-01-21 01:03:21 +0000

    add reflect-text-view, for testing with GtkTextView for sanity's sake.
    * src/Makefile.am, src/reflect.c: add reflect-text-view, for testing with
    	GtkTextView for sanity's sake.

--- src/reflect.c
+++ src/reflect.c
@@ -24,14 +24,76 @@
 #include <unistd.h>
 #include <gtk/gtk.h>
 #include <atk/atk.h>
-
-static GArray *contents = NULL;
-
+#ifdef USE_VTE
+#include "vte.h"
+#endif
 #ifdef USE_ZVT
 #ifdef HAVE_ZVT
 #include <libzvt/libzvt.h>
+#endif
+#endif
+
+static GArray *contents = NULL;
+
+#ifdef USE_TEXT_VIEW
+/*
+ * Implementation for a TextView widget.
+ */
 static void
-terminal_hint(GtkWidget *widget, gpointer data)
+terminal_init_text_view(GtkWidget **widget)
+{
+	*widget = gtk_text_view_new();
+	gtk_text_view_set_editable(GTK_TEXT_VIEW(*widget), TRUE);
+}
+static void
+terminal_shell_text_view(GtkWidget *widget)
+{
+	/* no-op */
+}
+static GtkAdjustment *
+terminal_adjustment_text_view(GtkWidget *terminal)
+{
+	return (GTK_TEXT_VIEW(terminal))->vadjustment;
+}
+#endif
+#ifdef USE_VTE
+/*
+ * Implementation for a VteTerminal widget.
+ */
+static void
+terminal_init_vte(GtkWidget **terminal)
+{
+	*terminal = vte_terminal_new();
+	g_signal_connect(G_OBJECT(*terminal), "eof",
+			 G_CALLBACK(gtk_main_quit), NULL);
+	g_signal_connect(G_OBJECT(*terminal), "child-exited",
+			 G_CALLBACK(gtk_main_quit), NULL);
+}
+static void
+terminal_shell_vte(GtkWidget *terminal)
+{
+	vte_terminal_fork_command(VTE_TERMINAL(terminal),
+				  getenv("SHELL") ? getenv("SHELL") : "/bin/sh",
+				  NULL,
+				  NULL,
+				  g_get_home_dir() ? g_get_home_dir() : NULL,
+				  FALSE,
+				  FALSE,
+				  FALSE);
+}
+static GtkAdjustment *
+terminal_adjustment_vte(GtkWidget *terminal)
+{
+	return (VTE_TERMINAL(terminal))->adjustment;
+}
+#endif
+#ifdef USE_ZVT
+#ifdef HAVE_ZVT
+/*
+ * Implementation for a ZvtTerm widget.
+ */
+static void
+terminal_hint_zvt(GtkWidget *widget, gpointer data)
 {
 	ZvtTerm *terminal;
 	GtkStyle *style;
@@ -62,15 +124,14 @@ terminal_hint(GtkWidget *widget, gpointer data)
 	gtk_widget_queue_resize(widget);
 }
 static void
-terminal_init(GtkWidget **terminal, GtkWidget **vscroll)
+terminal_init_zvt(GtkWidget **terminal)
 {
 	*terminal = zvt_term_new();
-	*vscroll = gtk_vscrollbar_new((ZVT_TERM(*terminal))->adjustment);
 	g_signal_connect_after(G_OBJECT(*terminal), "realize",
-			       G_CALLBACK(terminal_hint), NULL);
+			       G_CALLBACK(terminal_hint_zvt), NULL);
 }
 static void
-terminal_shell(GtkWidget *terminal)
+terminal_shell_zvt(GtkWidget *terminal)
 {
 	const char *shell;
 	shell = getenv("SHELL") ? getenv("SHELL") : "/bin/sh";
@@ -81,44 +142,34 @@ terminal_shell(GtkWidget *terminal)
 		g_assert_not_reached();
 	}
 }
-#else
-static void
-terminal_init(GtkWidget **terminal, GtkWidget **vscroll)
+static GtkAdjustment *
+terminal_adjustment_zvt(GtkWidget *terminal)
 {
-	/* We built the ZVT version, but we don't have ZVT. Bail. */
-	g_assert_not_reached();
+	return (ZVT_TERM(terminal))->adjustment;
+}
+#else
+/*
+ * Implementation for broken setups.
+ */
+static void
+terminal_init_broken(GtkWidget **terminal)
+{
+	g_error("libzvt not found at compile-time");
+	_exit(1);
 }
 static void
-terminal_shell(GtkWidget *terminal)
+terminal_shell_broken(GtkWidget *terminal)
 {
-	/* We built the ZVT version, but we don't have ZVT. Bail. */
-	g_assert_not_reached();
+	g_error("libzvt not found at compile-time");
+	_exit(1);
+}
+static GtkAdjustment *
+terminal_adjustment_broken(GtkWidget *terminal)
+{
+	g_error("libzvt not found at compile-time");
+	_exit(1);
 }
 #endif
-#else
-#include "vte.h"
-static void
-terminal_init(GtkWidget **terminal, GtkWidget **vscroll)
-{
-	*terminal = vte_terminal_new();
-	*vscroll = gtk_vscrollbar_new((VTE_TERMINAL(*terminal))->adjustment);
-	g_signal_connect(G_OBJECT(*terminal), "eof",
-			 G_CALLBACK(gtk_main_quit), NULL);
-	g_signal_connect(G_OBJECT(*terminal), "child-exited",
-			 G_CALLBACK(gtk_main_quit), NULL);
-}
-static void
-terminal_shell(GtkWidget *terminal)
-{
-	vte_terminal_fork_command(VTE_TERMINAL(terminal),
-				  getenv("SHELL") ? getenv("SHELL") : "/bin/sh",
-				  NULL,
-				  NULL,
-				  g_get_home_dir() ? g_get_home_dir() : NULL,
-				  FALSE,
-				  FALSE,
-				  FALSE);
-}
 #endif
 
 /*
@@ -129,7 +180,6 @@ update_contents(AtkObject *obj, GtkWidget *widget)
 {
 	int caret, i;
 	GString *s;
-	GtkTextBuffer *buffer;
 	caret = atk_text_get_caret_offset(ATK_TEXT(obj));
 	s = g_string_new("");
 	for (i = 0; i < contents->len; i++) {
@@ -143,10 +193,6 @@ update_contents(AtkObject *obj, GtkWidget *widget)
 	}
 	if (i == caret) {
 		s = g_string_append(s, "[CARET]");
-	}
-	if (GTK_IS_TEXT_VIEW(widget)) {
-		buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(widget));
-		gtk_text_buffer_set_text(buffer, s->str, s->len);
 	}
 	if (GTK_IS_LABEL(widget)) {
 		gtk_label_set_text(GTK_LABEL(widget), s->str);
@@ -182,14 +228,6 @@ text_changed_insert(AtkObject *obj, gint offset, gint length, gpointer data)
 		i++;
 	}
 
-	if (*p != '\0') {
-		g_warning("%d unprocessed bytes\n", strlen(p));
-	}
-	if (i != length) {
-		g_warning("%d unprocessed insertions\n", length - i);
-	}
-	g_assert((i == length) && (*p == '\0'));
-
 	g_free(inserted);
 
 	update_contents(obj, GTK_WIDGET(data));
@@ -212,61 +250,111 @@ text_caret_moved(AtkObject *obj, gint offset, gpointer data)
 	update_contents(obj, GTK_WIDGET(data));
 }
 
+/* Wrapper versions. */
+static void
+terminal_init(GtkWidget **terminal)
+{
+	*terminal = NULL;
+#ifdef USE_ZVT
+#ifdef HAVE_ZVT
+	terminal_init_zvt(terminal);
+	return;
+#else
+	terminal_init_broken(terminal);
+	return;
+#endif
+#endif
+#ifdef USE_TEXT_VIEW
+	terminal_init_text_view(terminal);
+	return;
+#endif
+#ifdef USE_VTE
+	terminal_init_vte(terminal);
+	return;
+#endif
+	g_assert_not_reached();
+}
+static void
+terminal_shell(GtkWidget *terminal)
+{
+#ifdef USE_ZVT
+#ifdef HAVE_ZVT
+	terminal_shell_zvt(terminal);
+	return;
+#else
+	terminal_shell_broken(terminal);
+	return;
+#endif
+#endif
+#ifdef USE_TEXT_VIEW
+	terminal_shell_text_view(terminal);
+	return;
+#endif
+#ifdef USE_VTE
+	terminal_shell_vte(terminal);
+	return;
+#endif
+	g_assert_not_reached();
+}
+static GtkAdjustment *
+terminal_adjustment(GtkWidget *terminal)
+{
+#ifdef USE_ZVT
+#ifdef HAVE_ZVT
+	return terminal_adjustment_zvt(terminal);
+#else
+	return terminal_adjustment_broken(terminal);
+#endif
+#endif
+#ifdef USE_TEXT_VIEW
+	return terminal_adjustment_text_view(terminal);
+#endif
+#ifdef USE_VTE
+	return terminal_adjustment_vte(terminal);
+#endif
+	g_assert_not_reached();
+}
+
 int
 main(int argc, char **argv)
 {
-	GtkWidget *text, *texttable, *terminal, *termbox, *termbox2, *pane;
-	GtkWidget *texthscroll, *textvscroll, *termvscroll, *window;
+	GtkWidget *label, *terminal, *tophalf, *pane, *window, *scrollbar;
 	AtkObject *obj;
+
 	gtk_init(&argc, &argv);
 
 	contents = g_array_new(TRUE, FALSE, sizeof(gunichar));
 
-	terminal_init(&terminal, &termvscroll);
+	terminal_init(&terminal);
 
-	termbox = gtk_hbox_new(FALSE, 0);
-	gtk_box_pack_start(GTK_BOX(termbox), terminal, TRUE, TRUE, 0);
-	gtk_box_pack_start(GTK_BOX(termbox), termvscroll, FALSE, FALSE, 0);
-	gtk_widget_show(terminal);
-	gtk_widget_show(termvscroll);
-	termbox2 = gtk_hbox_new(FALSE, 0);
-	gtk_box_pack_start(GTK_BOX(termbox2), termbox, FALSE, FALSE, 0);
-	gtk_widget_show(termbox);
-
-#ifdef USE_GTK_TEXT_VIEW
-	text = gtk_text_view_new();
-	gtk_text_view_set_editable(GTK_TEXT_VIEW(text), FALSE);
-	texthscroll = gtk_hscrollbar_new((GTK_TEXT_VIEW(text))->hadjustment);
-	textvscroll = gtk_vscrollbar_new((GTK_TEXT_VIEW(text))->vadjustment);
+#ifdef USE_TEXT_VIEW
+	tophalf = gtk_scrolled_window_new(NULL, terminal_adjustment(terminal));
+	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(tophalf),
+				       GTK_POLICY_AUTOMATIC,
+				       GTK_POLICY_AUTOMATIC);
+	scrollbar = NULL;
+	gtk_container_add(GTK_CONTAINER(tophalf), terminal);
 #else
-	text = gtk_label_new("");
-	gtk_label_set_justify(GTK_LABEL(text), GTK_JUSTIFY_LEFT);
-	gtk_misc_set_alignment(GTK_MISC(text), 0, 0);
-	texthscroll = NULL;
-	textvscroll = NULL;
+	tophalf = gtk_hbox_new(FALSE, 0);
+
+	gtk_box_pack_start(GTK_BOX(tophalf), terminal, TRUE, TRUE, 0);
+	gtk_widget_show(terminal);
+
+	scrollbar = gtk_vscrollbar_new(terminal_adjustment(terminal));
+	gtk_box_pack_start(GTK_BOX(tophalf), scrollbar, FALSE, TRUE, 0);
+	gtk_widget_show(scrollbar);
 #endif
-	texttable = gtk_table_new((texthscroll != NULL) ? 2 : 1,
-				  (textvscroll != NULL) ? 2 : 1,
-				  FALSE);
-	gtk_table_attach(GTK_TABLE(texttable), text, 0, 1, 0, 1,
-			 GTK_EXPAND | GTK_FILL, GTK_EXPAND | GTK_FILL, 0, 0);
-	gtk_widget_show(text);
-	if (textvscroll) {
-		gtk_table_attach(GTK_TABLE(texttable), textvscroll, 1, 2, 0, 1,
-				 GTK_SHRINK, GTK_EXPAND | GTK_FILL, 0, 0);
-		gtk_widget_show(textvscroll);
-	}
-	if (texthscroll) {
-		gtk_table_attach(GTK_TABLE(texttable), texthscroll, 0, 1, 1, 2,
-				 GTK_EXPAND | GTK_FILL, GTK_SHRINK, 0, 0);
-		gtk_widget_show(texthscroll);
-	}
+	gtk_widget_show(terminal);
+
+	label = gtk_label_new("");
+	gtk_label_set_justify(GTK_LABEL(label), GTK_JUSTIFY_LEFT);
+	gtk_misc_set_alignment(GTK_MISC(label), 0, 0);
 
 	pane = gtk_vpaned_new();
-	gtk_paned_add1(GTK_PANED(pane), termbox2);
-	gtk_paned_add2(GTK_PANED(pane), texttable);
-	gtk_widget_show(termbox2);
-	gtk_widget_show(texttable);
+	gtk_paned_pack1(GTK_PANED(pane), tophalf, TRUE, FALSE);
+	gtk_paned_pack2(GTK_PANED(pane), label, TRUE, FALSE);
+	gtk_widget_show(tophalf);
+	gtk_widget_show(label);
 
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	g_signal_connect(G_OBJECT(window), "delete_event",
@@ -274,20 +362,19 @@ main(int argc, char **argv)
 	gtk_container_add(GTK_CONTAINER(window), pane);
 	gtk_widget_show(pane);
 
-#ifdef USE_ZVT
-	gtk_widget_set_usize(terminal, 80 * 7, 25 * 13);
-#endif
 	terminal_shell(terminal);
 
 	obj = gtk_widget_get_accessible(terminal);
+	g_assert(obj != NULL);
 	g_signal_connect(G_OBJECT(obj), "text-changed::insert",
-			 G_CALLBACK(text_changed_insert), text);
+			 G_CALLBACK(text_changed_insert), label);
 	g_signal_connect(G_OBJECT(obj), "text-changed::delete",
-			 G_CALLBACK(text_changed_delete), text);
+			 G_CALLBACK(text_changed_delete), label);
 	g_signal_connect(G_OBJECT(obj), "text-caret-moved",
-			 G_CALLBACK(text_caret_moved), text);
+			 G_CALLBACK(text_caret_moved), label);
 
-	gtk_window_present(GTK_WINDOW(window));
+	gtk_window_set_default_size(GTK_WINDOW(window), 600, 450);
+	gtk_widget_show(window);
 
 	gtk_main();
 
