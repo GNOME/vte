a07f9ce78ff954124ab94846f2c5ac57ddfe91d5
Nalin Dahyabhai <nalin@src.gnome.org>
2003-07-04 03:55:33 +0000

    return "" instead of "xterm" in response to the ENQ control sequence. From
    * src/vte.c(vte_sequence_handler_return_terminal_status): return "" instead
    	of "xterm" in response to the ENQ control sequence.  From HEAD.

--- src/vte.c
+++ src/vte.c
@@ -4062,7 +4062,7 @@ vte_sequence_handler_return_terminal_status(VteTerminal *terminal,
 					    GQuark match_quark,
 					    GValueArray *params)
 {
-	vte_terminal_feed_child(terminal, "xterm", -1);
+	vte_terminal_feed_child(terminal, "", -1);
 }
 
 /* Send primary device attributes. */
