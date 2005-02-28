cea8ff59881258d16942aa6402c0ab10e110e4c8
Kjartan Maraas <kmaraas@gnome.org>
2005-02-28 21:38:59 +0000

    Patch from Mariano to make vte claim to be the right type terminal. Closes
    2005-02-28  Kjartan Maraas  <kmaraas@gnome.org>
    
    	* src/vte.c: (vte_sequence_handler_send_primary_device_attributes):
    	Patch from Mariano to make vte claim to be the right type terminal.
    	Closes bug #130671.

--- src/vte.c
+++ src/vte.c
@@ -4434,7 +4434,7 @@ vte_sequence_handler_send_primary_device_attributes(VteTerminal *terminal,
 						    GValueArray *params)
 {
 	/* Claim to be a VT220 with only national character set support. */
-	vte_terminal_feed_child(terminal, "[?60;9;c", -1);
+	vte_terminal_feed_child(terminal, "[?62;9;c", -1);
 	return FALSE;
 }
 
