1613c7756b040bf1ce49f04d71ecc7c1eb8967b3
Kjartan Maraas <kmaraas@gnome.org>
2005-03-14 14:33:35 +0000

    Back out one of the fedora patches since it apparently causes
    2005-03-14  Kjartan Maraas  <kmaraas@gnome.org>
    
    	* src/vte.c: (vte_terminal_scroll_region),
    	(vte_sequence_handler_al), (vte_sequence_handler_dl),
    	(vte_sequence_handler_sr), (vte_sequence_handler_insert_lines),
    	(vte_sequence_handler_delete_lines), (vte_terminal_handle_scroll):
    	Back out one of the fedora patches since it apparently causes

--- src/vte.c
+++ src/vte.c
@@ -707,14 +707,6 @@ vte_terminal_scroll_region(VteTerminal *terminal,
 	    (count == terminal->row_count) &&
 	    (terminal->pvt->scroll_lock_count == 0)) {
 		height = terminal->char_height;
-		
-		/* If we scroll with invalid areas, we will copy
-		 * invalid contents around the screen. So, we must
-		 * handle pending redraws first.
-		 */
-		gdk_window_process_updates (GTK_WIDGET(terminal)->window,
-					    FALSE);
-
 		gdk_window_scroll((GTK_WIDGET(terminal))->window,
 				  0, delta * height);
 		if (delta > 0) {
@@ -2281,9 +2273,6 @@ vte_sequence_handler_al(VteTerminal *terminal,
 		param = g_value_get_long(value);
 	}
 
-	/* Update the display. */
-	vte_terminal_scroll_region(terminal, start, end - start + 1, param);
-
 	/* Insert the right number of lines. */
 	for (i = 0; i < param; i++) {
 		/* Clear a line off the end of the region and add one to the
@@ -2300,6 +2289,9 @@ vte_sequence_handler_al(VteTerminal *terminal,
 		/* Adjust the scrollbars if necessary. */
 		vte_terminal_adjust_adjustments(terminal, FALSE);
 	}
+
+	/* Update the display. */
+	vte_terminal_scroll_region(terminal, start, end - start + 1, param);
 
 	/* We've modified the display.  Make a note of it. */
 	terminal->pvt->text_deleted_count++;
@@ -2917,9 +2909,6 @@ vte_sequence_handler_dl(VteTerminal *terminal,
 		param = g_value_get_long(value);
 	}
 
-	/* Update the display. */
-	vte_terminal_scroll_region(terminal, start, end - start + 1, -param);
-
 	/* Delete the right number of lines. */
 	for (i = 0; i < param; i++) {
 		/* Clear a line off the end of the region and add one to the
@@ -2929,6 +2918,9 @@ vte_sequence_handler_dl(VteTerminal *terminal,
 		/* Adjust the scrollbars if necessary. */
 		vte_terminal_adjust_adjustments(terminal, FALSE);
 	}
+
+	/* Update the display. */
+	vte_terminal_scroll_region(terminal, start, end - start + 1, -param);
 
 	/* We've modified the display.  Make a note of it. */
 	terminal->pvt->text_deleted_count++;
@@ -3880,12 +3872,12 @@ vte_sequence_handler_sr(VteTerminal *terminal,
 	}
 
 	if (screen->cursor_current.row == start) {
-		/* Update the display. */
-		vte_terminal_scroll_region(terminal, start, end - start + 1, 1);
 		/* If we're at the top of the scrolling region, add a
 		 * line at the top to scroll the bottom off. */
 		vte_remove_line_internal(terminal, end);
 		vte_insert_line_internal(terminal, start);
+		/* Update the display. */
+		vte_terminal_scroll_region(terminal, start, end - start + 1, 1);
 		vte_invalidate_cells(terminal,
 				     0, terminal->column_count,
 				     start, 2);
@@ -5432,8 +5424,6 @@ vte_sequence_handler_insert_lines(VteTerminal *terminal,
 	} else {
 		end = screen->insert_delta + terminal->row_count - 1;
 	}
-	/* Update the display. */
-	vte_terminal_scroll_region(terminal, row, end - row + 1, param);
 	/* Insert the new lines at the cursor. */
 	for (i = 0; i < param; i++) {
 		/* Clear a line off the end of the region and add one to the
@@ -5448,6 +5438,8 @@ vte_sequence_handler_insert_lines(VteTerminal *terminal,
 				 &screen->fill_defaults,
 				 terminal->column_count);
 	}
+	/* Update the display. */
+	vte_terminal_scroll_region(terminal, row, end - row + 1, param);
 	/* Adjust the scrollbars if necessary. */
 	vte_terminal_adjust_adjustments(terminal, FALSE);
 	/* We've modified the display.  Make a note of it. */
@@ -5483,8 +5475,6 @@ vte_sequence_handler_delete_lines(VteTerminal *terminal,
 	} else {
 		end = screen->insert_delta + terminal->row_count - 1;
 	}
-	/* Update the display. */
-	vte_terminal_scroll_region(terminal, row, end - row + 1, -param);
 	/* Clear them from below the current cursor. */
 	for (i = 0; i < param; i++) {
 		/* Insert a line at the end of the region and remove one from
@@ -5499,6 +5489,8 @@ vte_sequence_handler_delete_lines(VteTerminal *terminal,
 				 &screen->fill_defaults,
 				 terminal->column_count);
 	}
+	/* Update the display. */
+	vte_terminal_scroll_region(terminal, row, end - row + 1, -param);
 	/* Adjust the scrollbars if necessary. */
 	vte_terminal_adjust_adjustments(terminal, FALSE);
 	/* We've modified the display.  Make a note of it. */
@@ -10954,9 +10946,9 @@ vte_terminal_handle_scroll(VteTerminal *terminal)
 		    fprintf(stderr, "Scrolling by %ld\n", dy);
 	      }
 #endif
+		vte_terminal_match_contents_clear(terminal);
 		vte_terminal_scroll_region(terminal, screen->scroll_delta,
 					   terminal->row_count, -dy);
-		vte_terminal_match_contents_clear(terminal);
 		vte_terminal_emit_text_scrolled(terminal, dy);
 		vte_terminal_emit_contents_changed(terminal);
 	}
