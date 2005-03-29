139d9029a5182ff25a3989d27af5705fe18ab8fd
Kjartan Maraas <kmaraas@gnome.org>
2005-03-29 17:38:13 +0000

    Use head -n 1 instead of head -1 to avoid warning about the latter being
    2005-03-29  Kjartan Maraas  <kmaraas@gnome.org>
    
    	* autogen.sh: Use head -n 1 instead of head -1 to avoid
    	warning about the latter being deprecated in newer versions.
    	Reported by Ali Akcaagac. Closes bug #156022.
    	* vte.spec: Bump version

--- autogen.sh
+++ autogen.sh
@@ -34,7 +34,7 @@ WANT_AUTOCONF_2_5=1
 export WANT_AUTOCONF_2_5
 for autoconf in autoconf autoconf-2.59 autoconf-2.58 autoconf-2.57 autoconf-2.56 autoconf-2.55 autoconf-2.54 autoconf-2.53 autoconf-2.52 autoconf-2.51 autoconf-2.50 autoconf-2.5 ; do
 	if "$autoconf" --version < /dev/null > /dev/null 2>&1 ; then
-		version=`"$autoconf" --version | head -1 | awk '{print $NF}'`
+		version=`"$autoconf" --version | head -n 1 | awk '{print $NF}'`
 		acmajor=`echo "$version" | cut -f1 -d.`
 		acminor=`echo "$version" | cut -f2 -d.`
 		if test "$acmajor" -ge 3 ; then
