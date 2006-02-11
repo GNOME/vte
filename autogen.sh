e95ea25427129cc711c2b7337a9f0b502a24ede7
Behdad Esfahbod <behdad@gnome.org>
2006-02-11 04:45:33 +0000

    Update the build infrastructure. make distcheck works now!
    2006-02-10  Behdad Esfahbod  <behdad@gnome.org>
    
            Update the build infrastructure.  make distcheck works now!
    
            * autogen.sh: Use a generic autogen.sh that uses gnome-common.
    
            * configure.in: Simplify gtk-doc stuff.  Update to newer syntax.
    
            * Makefile.am, src/Makefile.am, doc/reference/Makefile.am: Updated.

--- autogen.sh
+++ autogen.sh
@@ -4,115 +4,20 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-ORIGDIR=`pwd`
-cd $srcdir
-PROJECT=vte
-TEST_TYPE=-f
+REQUIRED_AUTOMAKE_VERSION=1.8
 
-DIE=0
+PKG_NAME="vte"
 
-have_libtool=false
-if libtool --version < /dev/null > /dev/null 2>&1 ; then
-	libtool_version=`libtoolize --version |  libtoolize --version | sed 's/^[^0-9]*\([0-9.][0-9.]*\).*/\1/'`
-	case $libtool_version in
-	    1.4*|1.5*)
-		have_libtool=true
-		;;
-	esac
-fi
-
-if $have_libtool ; then : ; else
-	echo
-	echo "You must have libtool 1.4 or newer installed to compile $PROJECT."
-	echo "Install the appropriate package for your distribution,"
-	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-	DIE=1
-fi
-
-# Tell the Mandrake autoconf wrapper to prefer autoconf 2.5.
-WANT_AUTOCONF_2_5=1
-export WANT_AUTOCONF_2_5
-for autoconf in autoconf autoconf-2.59 autoconf-2.58 autoconf-2.57 autoconf-2.56 autoconf-2.55 autoconf-2.54 autoconf-2.53 autoconf-2.52 autoconf-2.51 autoconf-2.50 autoconf-2.5 ; do
-	if "$autoconf" --version < /dev/null > /dev/null 2>&1 ; then
-		version=`"$autoconf" --version | head -n 1 | awk '{print $NF}'`
-		acmajor=`echo "$version" | cut -f1 -d.`
-		acminor=`echo "$version" | cut -f2 -d.`
-		if test "$acmajor" -ge 3 ; then
-			break
-		fi
-		if test "$acmajor" -ge 2 ; then
-			if test "$acminor" -ge 50 ; then
-				break
-			fi
-		fi
-	fi
-done
-autoheader=`echo "$autoconf" | sed -e s,autoconf,autoheader,g`
-if ! "$autoconf" --version < /dev/null > /dev/null 2>&1 ; then
-	echo
-	echo "You must have autoconf 2.52 installed to compile $PROJECT."
-	echo "Install the appropriate package for your distribution,"
-	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-	DIE=1
-fi
-autoheader=`echo "$autoconf" | sed s,autoconf,autoheader,g`
-
-(freetype-config --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have freetype 2 installed to compile $PROJECT."
-	echo "Install the appropriate package for your distribution, or get the"
-	echo "source tarball at ftp://ftp.freetype.org/freetype/freetype2"
-	DIE=1
+(test -f $srcdir/configure.in \
+  && test -f $srcdir/README \
+  && test -d $srcdir/src) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
 }
 
-have_automake=false
-for automakev in 1.9 1.8 1.7 1.6 ; do
-	if automake-$automakev --version < /dev/null > /dev/null 2>&1 ; then
-		have_automake=true
-		break;
-	fi
-done
-if $have_automake ; then : ; else
-	echo
-	echo "You must have automake 1.6 installed to compile $PROJECT."
-	echo "Get ftp://ftp.gnu.org/pub/gnu/automake/automake-1.6.tar.gz"
-	echo "(or a newer version if it is available)"
-	DIE=1
-fi
-
-if test "$DIE" -eq 1; then
-	exit 1
-fi
-
-if test -z "$AUTOGEN_SUBDIR_MODE"; then
-        if test -z "$*"; then
-                echo "I am going to run ./configure with no arguments - if you wish "
-                echo "to pass any to it, please specify them on the $0 command line."
-        fi
-fi
-
-case $CC in
-*xlc | *xlc\ * | *lcc | *lcc\ *) am_opt=--include-deps;;
-esac
-
-libtoolize -f -c
-glib-gettextize -f -c
-aclocal-$automakev $ACLOCAL_FLAGS
-$autoheader
-automake-$automakev -a -c $am_opt
-$autoconf
-
-cd gnome-pty-helper
-aclocal-$automakev $ACLOCAL_FLAGS
-$autoheader
-automake-$automakev -a -c $am_opt
-$autoconf
-
-cd $ORIGDIR
-
-if test -z "$AUTOGEN_SUBDIR_MODE"; then
-        $srcdir/configure --enable-maintainer-mode --enable-gtk-doc "$@"
-	chmod -Rf u+w $srcdir
-        echo 
-        echo "Now type 'make' to compile $PROJECT."
-fi
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common from the GNOME CVS"
+    exit 1
+}
+USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
