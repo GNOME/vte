14eda4d86401762c88040af70c4259a09b6c4614
Behdad Esfahbod <behdad@gnome.org>
2006-06-07 16:46:39 +0000

    Bug 339529 – gnome-terminal (vte) crashes when detatched window is
    2006-06-07  Behdad Esfahbod  <behdad@gnome.org>
    
            Bug 339529 – gnome-terminal (vte) crashes when detatched window is
            closed
    
            * src/vte.c (vte_invalidate_region): Bail out if terminal->window is
            NULL.

--- src/vte.c
+++ src/vte.c
@@ -198,7 +198,10 @@ _vte_terminal_set_default_attributes(VteTerminal *terminal)
 static gboolean
 vte_invalidate_region(VteTerminal *terminal)
 {
-	if (!GTK_WIDGET_REALIZED(terminal))
+	if (G_UNLIKELY (!GTK_WIDGET_REALIZED(terminal)))
+		return FALSE;
+
+	if (G_UNLIKELY (GTK_WIDGET(terminal)->window == NULL))
 		return FALSE;
 
 	if (!terminal->pvt->update_region)
