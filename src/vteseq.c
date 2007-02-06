e8788fb343b122f77dcb087c3644633d59649fbe
Chris Wilson <chris@chris-wilson.co.uk>
2007-02-06 21:47:58 +0000

    Bug 342059 – ASCII escape sequences don't work as expected Patch by
    2007-02-06  Chris Wilson  <chris@chris-wilson.co.uk>
    
    	Bug 342059 – ASCII escape sequences don't work as expected
    	Patch by Mariano Suárez-Alvarez.
    
    	* src/vteseq.c: (vte_sequence_handler_cursor_character_absolute):
    		Provide a default value.
    
    
    svn path=/trunk/; revision=1640

--- src/vteseq.c
+++ src/vteseq.c
@@ -2886,8 +2886,25 @@ vte_sequence_handler_cursor_character_absolute(VteTerminal *terminal,
 					       GQuark match_quark,
 					       GValueArray *params)
 {
-	return vte_sequence_handler_offset(terminal, match, match_quark, params,
-					   -1, vte_sequence_handler_ch);
+	VteScreen *screen;
+	GValue *value;
+	long val;
+
+	screen = terminal->pvt->screen;
+	
+        val = 0;
+	if ((params != NULL) && (params->n_values > 0)) {
+		value = g_value_array_get_nth(params, 0);
+		if (G_VALUE_HOLDS_LONG(value)) {
+			val = CLAMP(g_value_get_long(value),
+				    0,
+				    terminal->column_count - 1);
+		}
+	}
+
+        screen->cursor_current.col = val;
+
+	return FALSE;
 }
 
 /* Move the cursor to the given position, 1-based. */
