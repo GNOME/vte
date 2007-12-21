db18d22ff40b71fd78236f3e549dd17c6040088d
Chris Wilson <chris@chris-wilson.co.uk>
2007-12-21 08:56:12 +0000

    Bug 503164 – Drawing problems for VTE with gtk+-quartz
    2007-12-21  Chris Wilson  <chris@chris-wilson.co.uk>
    
        Bug 503164 – Drawing problems for VTE with gtk+-quartz
    
        * src/vte.c (vte_terminal_realize):
        Continuing the saga, remove the fiddling with visibility state
        during realize.
    
    
    svn path=/trunk/; revision=2015

--- src/vte.c
+++ src/vte.c
@@ -8055,9 +8055,6 @@ vte_terminal_realize(GtkWidget *widget)
 	/* Clear modifiers. */
 	terminal->pvt->modifiers = 0;
 
-	/* window is obscured until mapped */
-	terminal->pvt->visibility_state = GDK_VISIBILITY_FULLY_OBSCURED;
-
 	/* Create our invisible cursor. */
 	bitmap = gdk_bitmap_create_from_data(widget->window, "\0", 1, 1);
 	terminal->pvt->mouse_inviso_cursor = gdk_cursor_new_from_pixmap(bitmap,
