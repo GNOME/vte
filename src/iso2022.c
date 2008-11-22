GNOME/vte

commit 2ddb77285675c657d704e4bfb1431167088b4fff

cf Bug 83285 – Treacle-slow scrolling in gnome-terminal on unaccelerated
2007-02-04  Chris Wilson  <chris@chris-wilson.co.uk>

	cf Bug 83285 – Treacle-slow scrolling in gnome-terminal on unaccelerated X server

	Another chunk of performance improvements - the majority focussing on
	reducing the amount of work down per interrupt (input data from
	child).

	To handle reads without addition copies VTE_INPUT_CHUNK_SIZE chunks
	are allocated on demand and filled from read with no further copy -
	previously the read was into a local buffer and then immediately
	copied into a _vte_buffer.
	_vte_iso2022_process() is then adjusted to use a ptr+length, rather
	than a _vte_buffer and is called for every input chunk (taking special
	care over the boundary cases).

	* src/interpret.c: (main):
	* src/iso2022.c: (_vte_iso2022_fragment_input),
	(process_8_bit_sequence), (process_cdata), (_vte_iso2022_process):
	* src/iso2022.h:
	* src/vte-private.h:
	* src/vte.c: (get_chunk), (release_chunk), (prune_chunks),
	(_vte_incoming_chunks_release), (_vte_incoming_chunks_length),
	(_vte_incoming_chunks_count), (_vte_incoming_chunks_reverse),
	(vte_terminal_match_check_internal), (_vte_terminal_ensure_cursor),
	(vte_terminal_set_colors), (_vte_terminal_insert_char),
	(vte_terminal_catch_child_exited), (vte_terminal_eof),
	(vte_terminal_process_incoming), (_vte_terminal_feed_chunks),
	(vte_terminal_io_read), (vte_terminal_feed), (vte_terminal_send),
	(vte_terminal_init), (vte_terminal_finalize), (vte_terminal_reset),
	(remove_from_active_list), (need_processing), (update_regions),
	(update_repeat_timeout):
	* src/vte.h:
	* src/vteseq.c: (vte_sequence_handler_cb),
	(vte_sequence_handler_ce), (vte_sequence_handler_ec):



svn path=/trunk/; revision=1612

src/iso2022.c
@@ -1043,7 +1043,7 @@ _vte_iso2022_sequence_length(const unsigned char *nextctl, gsize length)
 }
 
 static GArray *
-_vte_iso2022_fragment_input(struct _vte_buffer *input)
+_vte_iso2022_fragment_input(guchar *input, gsize length)
 {
 	const guchar *nextctl, *p, *q;
 	glong sequence_length = 0;
@@ -1053,16 +1053,16 @@ _vte_iso2022_fragment_input(struct _vte_buffer *input)
 
 	blocks = g_array_new(FALSE, FALSE, sizeof(struct _vte_iso2022_block));
 
-	p = input->bytes;
-	q = input->bytes + _vte_buffer_length(input);
+	p = input;
+	q = input + length;
 	quit = FALSE;
-	while ((p < q) && !quit) {
+	while (p < q && !quit) {
 		nextctl = _vte_iso2022_find_nextctl(p, q - p);
 		if (nextctl == NULL) {
 			/* It's all garden-variety data. */
 			block.type = _vte_iso2022_cdata;
-			block.start = p - input->bytes;
-			block.end = q - input->bytes;
+			block.start = p - input;
+			block.end = q - input;
 			g_array_append_val(blocks, block);
 			/* Break out of the loop. */
 			break;
@@ -1070,8 +1070,8 @@ _vte_iso2022_fragment_input(struct _vte_buffer *input)
 		/* We got some garden-variety data. */
 		if (nextctl != p) {
 			block.type = _vte_iso2022_cdata;
-			block.start = p - input->bytes;
-			block.end = nextctl - input->bytes;
+			block.start = p - input;
+			block.end = nextctl - input;
 			g_array_append_val(blocks, block);
 		}
 		/* Move on to the control data. */
@@ -1082,8 +1082,8 @@ _vte_iso2022_fragment_input(struct _vte_buffer *input)
 		case -1:
 			/* It's just garden-variety data. */
 			block.type = _vte_iso2022_cdata;
-			block.start = p - input->bytes;
-			block.end = nextctl + 1 - input->bytes;
+			block.start = p - input;
+			block.end = nextctl + 1 - input;
 			g_array_append_val(blocks, block);
 			/* Continue at the next byte. */
 			p = nextctl + 1;
@@ -1091,8 +1091,8 @@ _vte_iso2022_fragment_input(struct _vte_buffer *input)
 		case 0:
 			/* Inconclusive.  Save this data and try again later. */
 			block.type = _vte_iso2022_preserve;
-			block.start = nextctl - input->bytes;
-			block.end = q - input->bytes;
+			block.start = nextctl - input;
+			block.end = q - input;
 			g_array_append_val(blocks, block);
 			/* Trigger an end-of-loop. */
 			quit = TRUE;
@@ -1100,8 +1100,8 @@ _vte_iso2022_fragment_input(struct _vte_buffer *input)
 		default:
 			/* It's a control sequence. */
 			block.type = _vte_iso2022_control;
-			block.start = nextctl - input->bytes;
-			block.end = nextctl + sequence_length - input->bytes;
+			block.start = nextctl - input;
+			block.end = nextctl + sequence_length - input;
 			g_array_append_val(blocks, block);
 			/* Continue after the sequence. */
 			p = nextctl + sequence_length;
@@ -1174,7 +1174,7 @@ process_8_bit_sequence(struct _vte_iso2022_state *state,
 			width = force_width;
 		} else {
 			if (_vte_iso2022_is_ambiguous(c)) {
-				width = _vte_iso2022_ambiguous_width(state);
+				width = state->ambiguous_width;
 			}
 		}
 		_vte_debug_print(VTE_DEBUG_SUBSTITUTION,
@@ -1194,7 +1194,7 @@ static glong
 process_cdata(struct _vte_iso2022_state *state, const guchar *cdata, gsize length,
 	      GArray *gunichars)
 {
-	int ambiguous_width = 0;
+	int ambiguous_width;
 	glong processed = 0;
 	GTree *map;
 	guint bytes_per_char, force_width, current;
@@ -1208,7 +1208,7 @@ process_cdata(struct _vte_iso2022_state *state, const guchar *cdata, gsize lengt
 	gpointer p;
 	gboolean single, stop;
 
-	ambiguous_width = _vte_iso2022_ambiguous_width(state);
+	ambiguous_width = state->ambiguous_width;
 
 	single = (state->override != -1);
 	current = (state->override != -1) ? state->override : state->current;
@@ -1612,9 +1612,9 @@ process_control(struct _vte_iso2022_state *state, guchar *ctl, gsize length,
 	}
 }
 
-void
+gsize
 _vte_iso2022_process(struct _vte_iso2022_state *state,
-		     struct _vte_buffer *input,
+		     guchar *input, gsize length,
 		     GArray *gunichars)
 {
 	GArray *blocks;
@@ -1622,7 +1622,7 @@ _vte_iso2022_process(struct _vte_iso2022_state *state,
 	gboolean preserve_last = FALSE;
 	guint i, initial;
 
-	blocks = _vte_iso2022_fragment_input(input);
+	blocks = _vte_iso2022_fragment_input(input, length);
 
 	for (i = 0; i < blocks->len; i++) {
 		block = &g_array_index(blocks, struct _vte_iso2022_block, i);
@@ -1633,14 +1633,14 @@ _vte_iso2022_process(struct _vte_iso2022_state *state,
 				g_printerr("%3ld %3ld CDATA \"%.*s\"",
 					block->start, block->end,
 					(int) (block->end - block->start),
-					input->bytes + block->start);
+					input + block->start);
 				g_printerr(" (");
 				for (j = block->start; j < block->end; j++) {
 					if (j > block->start) {
 						g_printerr(", ");
 					}
 					g_printerr("0x%02x",
-						input->bytes[j]);
+						input[j]);
 				}
 				g_printerr(")\n");
 			}
@@ -1648,7 +1648,7 @@ _vte_iso2022_process(struct _vte_iso2022_state *state,
 			while (initial < block->end - block->start) {
 				int j;
 				j = process_cdata(state,
-						  input->bytes +
+						  input +
 						  block->start +
 						  initial,
 						  block->end -
@@ -1673,7 +1673,7 @@ _vte_iso2022_process(struct _vte_iso2022_state *state,
 					"%3ld %3ld CONTROL ",
 					block->start, block->end);
 			process_control(state,
-					input->bytes + block->start,
+					input + block->start,
 					block->end - block->start,
 					gunichars);
 			preserve_last = FALSE;
@@ -1692,17 +1692,12 @@ _vte_iso2022_process(struct _vte_iso2022_state *state,
 	}
 	if (preserve_last && (blocks->len > 0)) {
 		block = &g_array_index(blocks, struct _vte_iso2022_block, blocks->len - 1);
-		_vte_debug_print(VTE_DEBUG_SUBSTITUTION,
-				"Consuming %ld bytes.\n", block->start);
-		_vte_buffer_consume(input, block->start);
-		g_assert(_vte_buffer_length(input) == block->end - block->start);
-	} else {
-		_vte_debug_print(VTE_DEBUG_SUBSTITUTION,
-				"Consuming %ld bytes.\n",
-				(long) _vte_buffer_length(input));
-		_vte_buffer_clear(input);
+		length = block->start;
 	}
 	g_array_free(blocks, TRUE);
+	_vte_debug_print(VTE_DEBUG_SUBSTITUTION,
+			"Consuming %ld bytes.\n", (long) length);
+	return length;
 }
 
 gssize

src/interpret.c
@@ -86,7 +86,8 @@ main(int argc, char **argv)
 	while (fread(&c, 1, 1, infile) == 1) {
 		_vte_buffer_append(buffer, &c, 1);
 	}
-	_vte_iso2022_process(subst, buffer, array);
+	_vte_iso2022_process(subst, buffer->bytes,
+			_vte_buffer_length(buffer), array);
 
 	i = 0;
 	while (i <= array->len) {

src/iso2022.h
@@ -38,8 +38,9 @@ struct _vte_iso2022_state *_vte_iso2022_state_new(const char *native_codeset,
 void _vte_iso2022_state_set_codeset(struct _vte_iso2022_state *state,
 				    const char *codeset);
 const char *_vte_iso2022_state_get_codeset(struct _vte_iso2022_state *state);
-void _vte_iso2022_process(struct _vte_iso2022_state *state,
-			  struct _vte_buffer *input, GArray *gunichars);
+gsize _vte_iso2022_process(struct _vte_iso2022_state *state,
+			  guchar *input, gsize length,
+			  GArray *gunichars);
 gunichar _vte_iso2022_process_single(struct _vte_iso2022_state *state,
 				     gunichar c, gunichar map);
 void _vte_iso2022_state_free(struct _vte_iso2022_state *);

src/vte-private.h
@@ -177,7 +177,12 @@ struct _VteTerminalPrivate {
 	/* Input data queues. */
 	const char *encoding;		/* the pty's encoding */
 	struct _vte_iso2022_state *iso2022;
-	struct _vte_buffer *incoming;	/* pending bytestream */
+	struct _vte_incoming_chunk{
+		struct _vte_incoming_chunk *next;
+		gint len;
+		guchar data[VTE_INPUT_CHUNK_SIZE
+			- sizeof(struct _vte_incoming_chunk *) - sizeof(gint)];
+	} *incoming;			/* pending bytestream */
 	GArray *pending;		/* pending characters */
 	GSList *update_regions;
 	gboolean invalidated_all;	/* pending refresh of entire terminal */
@@ -356,7 +361,7 @@ struct _VteTerminalPrivate {
 };
 
 
-void _vte_terminal_ensure_cursor(VteTerminal *terminal, gboolean current);
+VteRowData *_vte_terminal_ensure_cursor(VteTerminal *terminal, gboolean current);
 void _vte_terminal_set_pointer_visible(VteTerminal *terminal, gboolean visible);
 void _vte_invalidate_all(VteTerminal *terminal);
 void _vte_invalidate_cells(VteTerminal *terminal,

src/vte.h
@@ -351,7 +351,6 @@ char *vte_terminal_get_text_range(VteTerminal *terminal,
 				  GArray *attributes);
 void vte_terminal_get_cursor_position(VteTerminal *terminal,
 				      glong *column, glong *row);
-
 /* Display string matching:  clear all matching expressions. */
 void vte_terminal_match_clear_all(VteTerminal *terminal);
 

src/vteseq.c
@@ -1024,10 +1024,7 @@ vte_sequence_handler_cb(VteTerminal *terminal,
 	screen = terminal->pvt->screen;
 
 	/* Get the data for the row which the cursor points to. */
-	_vte_terminal_ensure_cursor(terminal, FALSE);
-	rowdata = _vte_ring_index(screen->row_data,
-				  VteRowData *,
-				  screen->cursor_current.row);
+	rowdata = _vte_terminal_ensure_cursor(terminal, FALSE);
 	/* Clear the data up to the current column with the default
 	 * attributes.  If there is no such character cell, we need
 	 * to add one. */
@@ -1131,9 +1128,7 @@ vte_sequence_handler_ce(VteTerminal *terminal,
 
 	screen = terminal->pvt->screen;
 	/* Get the data for the row which the cursor points to. */
-	_vte_terminal_ensure_cursor(terminal, FALSE);
-	rowdata = _vte_ring_index(screen->row_data, VteRowData *,
-				  screen->cursor_current.row);
+	rowdata = _vte_terminal_ensure_cursor(terminal, FALSE);
 	g_assert(rowdata != NULL);
 	/* Remove the data at the end of the array until the current column
 	 * is the end of the array. */
@@ -1143,7 +1138,7 @@ vte_sequence_handler_ce(VteTerminal *terminal,
 	/* Add enough cells to the end of the line to fill out the row. */
 	vte_g_array_fill(rowdata->cells,
 			 &screen->fill_defaults,
-			 terminal->column_count);
+			 terminal->column_count - screen->cursor_current.col);
 	rowdata->soft_wrapped = 0;
 	/* Repaint this row. */
 	_vte_invalidate_cells(terminal,
@@ -1645,12 +1640,8 @@ vte_sequence_handler_ec(VteTerminal *terminal,
 	}
 
 	/* Clear out the given number of characters. */
-	_vte_terminal_ensure_cursor(terminal, TRUE);
+	rowdata = _vte_terminal_ensure_cursor(terminal, TRUE);
 	if (_vte_ring_next(screen->row_data) > screen->cursor_current.row) {
-		/* Get the data for the row which the cursor points to. */
-		rowdata = _vte_ring_index(screen->row_data,
-					  VteRowData *,
-					  screen->cursor_current.row);
 		g_assert(rowdata != NULL);
 		/* Write over the characters.  (If there aren't enough, we'll
 		 * need to create them.) */

commit 98ebf0dfe62d16d23436723f145417f87a66b2ae

Make iso2022 programme build again; it was broken since r1612.
	* src/iso2022.c: (main): Make iso2022 programme build again; it was
	broken since r1612.

svn path=/trunk/; revision=2173

src/iso2022.c
@@ -1833,7 +1833,7 @@ main(int argc, char **argv)
 			}
 		}
 		_vte_buffer_append(buffer, string->str, string->len);
-		_vte_iso2022_process(state, buffer, gunichars);
+		_vte_iso2022_process(state, buffer, _vte_buffer_length (buffer), gunichars);
 		g_string_free(string, TRUE);
 	} else {
 		for (i = 0; i < G_N_ELEMENTS(strings); i++) {
@@ -1841,7 +1841,7 @@ main(int argc, char **argv)
 			_vte_buffer_append(buffer, string->str, string->len);
 			g_string_free(string, TRUE);
 			if (strings[i].process) {
-				_vte_iso2022_process(state, buffer, gunichars);
+				_vte_iso2022_process(state, buffer, _vte_buffer_length (buffer), gunichars);
 			}
 		}
 	}