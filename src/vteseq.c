491c7df4a12e10f59e0f91f7f4918af9e5a658f7
Behdad Esfahbod <behdad@src.gnome.org>
2008-12-01 09:30:29 +0000

    Minor.
    svn path=/trunk/; revision=2268

--- src/vteseq.c
+++ src/vteseq.c
@@ -3387,22 +3387,22 @@ _vte_sequence_get_handler (const char *name)
 
 /* Handle a terminal control sequence and its parameters. */
 void
-_vte_terminal_handle_sequence(VteTerminal *terminal2,
+_vte_terminal_handle_sequence(VteTerminal *terminal,
 			      const char *match_s,
 			      GQuark match G_GNUC_UNUSED,
-			      GValueArray *params2)
+			      GValueArray *params)
 {
 	VteTerminalSequenceHandler handler;
 
 	_VTE_DEBUG_IF(VTE_DEBUG_PARSE)
-		display_control_sequence(match_s, params2);
+		display_control_sequence(match_s, params);
 
 	/* Find the handler for this control sequence. */
 	handler = _vte_sequence_get_handler (match_s);
 
 	if (handler != NULL) {
 		/* Let the handler handle it. */
-		handler (terminal2, params2);
+		handler (terminal, params);
 	} else {
 		_vte_debug_print (VTE_DEBUG_MISC,
 				  "No handler for control sequence `%s' defined.\n",
