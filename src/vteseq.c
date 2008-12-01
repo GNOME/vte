6adba974637bc29a16693953cf231c7776913825
Behdad Esfahbod <behdad@gnome.org>
2008-12-01 03:46:11 +0000

    More code shufflling.
    2008-11-30  Behdad Esfahbod  <behdad@gnome.org>
    
            * src/vte-private.h:
            * src/vte.c:
            * src/vteseq.c (_vte_terminal_scroll_text),
            (vte_sequence_handler_scroll_down),
            (vte_sequence_handler_scroll_up):
            More code shufflling.
    
    
    svn path=/trunk/; revision=2250

--- src/vteseq.c
+++ src/vteseq.c
@@ -337,6 +337,58 @@ _vte_terminal_clear_above_current (VteTerminal *terminal)
 	terminal->pvt->text_deleted_flag = TRUE;
 }
 
+/* Scroll the text, but don't move the cursor.  Negative = up, positive = down. */
+static void
+_vte_terminal_scroll_text (VteTerminal *terminal, int scroll_amount)
+{
+	VteRowData *row, *old_row;
+	long start, end, i;
+	VteScreen *screen;
+
+	screen = terminal->pvt->screen;
+
+	if (screen->scrolling_restricted) {
+		start = screen->insert_delta + screen->scrolling_region.start;
+		end = screen->insert_delta + screen->scrolling_region.end;
+	} else {
+		start = screen->insert_delta;
+		end = start + terminal->row_count - 1;
+	}
+
+	old_row = terminal->pvt->free_row;
+	while (_vte_ring_next(screen->row_data) <= end) {
+		if (old_row) {
+			row = _vte_reset_row_data (terminal, old_row, FALSE);
+		} else {
+			row = _vte_new_row_data_sized(terminal, FALSE);
+		}
+		old_row = _vte_ring_append(terminal->pvt->screen->row_data, row);
+	}
+	terminal->pvt->free_row = old_row;
+	if (scroll_amount > 0) {
+		for (i = 0; i < scroll_amount; i++) {
+			vte_remove_line_internal(terminal, end);
+			vte_insert_line_internal(terminal, start);
+		}
+	} else {
+		for (i = 0; i < -scroll_amount; i++) {
+			vte_remove_line_internal(terminal, start);
+			vte_insert_line_internal(terminal, end);
+		}
+	}
+
+	/* Update the display. */
+	_vte_terminal_scroll_region(terminal, start, end - start + 1,
+				   scroll_amount);
+
+	/* Adjust the scrollbars if necessary. */
+	_vte_terminal_adjust_adjustments(terminal);
+
+	/* We've modified the display.  Make a note of it. */
+	terminal->pvt->text_inserted_flag = TRUE;
+	terminal->pvt->text_deleted_flag = TRUE;
+}
+
 
 
 
@@ -2100,7 +2152,7 @@ vte_sequence_handler_scroll_down(VteTerminal *terminal,
 		}
 	}
 
-	_vte_terminal_scroll (terminal, val);
+	_vte_terminal_scroll_text (terminal, val);
 	return FALSE;
 }
 
@@ -2122,7 +2174,7 @@ vte_sequence_handler_scroll_up(VteTerminal *terminal,
 		}
 	}
 
-	_vte_terminal_scroll (terminal, -val);
+	_vte_terminal_scroll_text (terminal, -val);
 	return FALSE;
 }
 
