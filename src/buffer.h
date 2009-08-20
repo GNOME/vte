dc03f2f6035befdc2e8ca5a0537ca2e8c26c324f
Behdad Esfahbod <behdad@behdad.org>
2009-08-20 15:15:01 -0400

    Update copyright header

--- src/buffer.h
+++ src/buffer.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2001,2002 Red Hat, Inc.
+ * Copyright 2009 Red Hat, Inc.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
