c56c3c4673a310f5d71bc16045a331d362c0bbc5
Behdad Esfahbod <behdad@behdad.org>
2010-08-19 12:52:50 -0400

    Revert "Bug 601926 - Don't hardcode meta to alt"
    This reverts commit b73782a28894e25ed146271f9d6c6775a6836199.

--- src/keymap.h
+++ src/keymap.h
@@ -27,7 +27,7 @@
 
 G_BEGIN_DECLS
 
-#define VTE_META_MASK		GDK_META_MASK
+#define VTE_META_MASK		GDK_MOD1_MASK
 #define VTE_NUMLOCK_MASK	GDK_MOD2_MASK
 
 /* Map the specified keyval/modifier setup, dependent on the mode, to either
