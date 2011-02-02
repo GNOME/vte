f46b5c288c9b1222cf614f22c1d1f53f1ee60c56
Christian Persch <chpe@gnome.org>
2011-02-02 17:49:06 +0100

    Revert "Use the recommended configure.ac instead configure.in"
    This reverts commit 765f1087eced3d19768a7ebd98500e5ff3df8a1a.
    
    Renaming breaks automatic reconfiguring from make during git bisect.

--- autogen.sh
+++ autogen.sh
@@ -6,7 +6,7 @@ test -z "$srcdir" && srcdir=.
 
 PKG_NAME="vte"
 
-(test -f $srcdir/configure.ac \
+(test -f $srcdir/configure.in \
   && test -f $srcdir/README \
   && test -d $srcdir/src) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
