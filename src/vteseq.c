feeee4b5832b17641e505b7083e0d299fdae318e
Christian Persch <chpe@gnome.org>
2012-05-19 19:36:09 +0200

    emulation: Limit integer arguments to 65535
    To guard against malicious sequences containing excessively big numbers,
    limit all parsed numbers to 16 bit range. Doing this here in the parsing
    routine is a catch-all guard; this doesn't preclude enforcing
    more stringent limits in the handlers themselves.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=676090

--- src/vteseq.c
+++ src/vteseq.c
@@ -557,7 +557,7 @@ vte_sequence_handler_multiple(VteTerminal *terminal,
                               GValueArray *params,
                               VteTerminalSequenceHandler handler)
 {
-        vte_sequence_handler_multiple_limited(terminal, params, handler, G_MAXLONG);
+        vte_sequence_handler_multiple_limited(terminal, params, handler, G_MAXUSHORT);
 }
 
 static void
