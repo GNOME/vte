0062b25d84cd86f04525059d1bfa1ee186acf140
Christian Persch <chpe@gnome.org>
2013-07-30 15:31:26 +0200

    build: Use autoreconf directly

--- autogen.sh
+++ autogen.sh
@@ -1,29 +1,16 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
+set -e
+
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="vte"
+cd "$srcdir"
+mkdir -p m4 >/dev/null 2>&1 || true
+gtkdocize --copy --flavour no-tmpl
+autoreconf --verbose --force --install
+intltoolize --force
+cd -
 
-(test -f $srcdir/configure.in \
-  && test -f $srcdir/README \
-  && test -d $srcdir/src) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level $PKG_NAME directory"
-    exit 1
-}
-
-echo "checking for GNU gperf"
-which gperf || {
-    echo "You need to install GNU gperf"
-    exit 1
-}
-
-echo "checking for gnome-autogen.sh"
-which gnome-autogen.sh || {
-    echo "You need to install the gnome-common package"
-    exit 1
-}
-
-. gnome-autogen.sh
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
