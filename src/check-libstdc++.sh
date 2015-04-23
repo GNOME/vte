5a58273b8ec252fad7a810614ed612a7e42f2a9b
Ting-Wei Lan <lantw@src.gnome.org>
2015-04-24 00:18:42 +0800

    build: Add libc++ check to check-libstdc++.sh
    https://bugzilla.gnome.org/show_bug.cgi?id=748376

--- src/check-libstdc++.sh
+++ src/check-libstdc++.sh
@@ -5,9 +5,11 @@ export LC_ALL=C
 LDD=ldd
 GREP=grep
 
-if $LDD .libs/libvte-$VTE_API_VERSION.so | $GREP 'libstdc++' &>/dev/null; then
-    echo "FAIL: libvte-$VTE_API_VERSION.so is linked to libstdc++"
-    exit 1
-fi
+for cxxlib in 'libstdc++' 'libc++'; do
+    if $LDD .libs/libvte-$VTE_API_VERSION.so | $GREP "$cxxlib" &>/dev/null; then
+        echo "FAIL: libvte-$VTE_API_VERSION.so is linked to $cxxlib"
+        exit 1
+    fi
+done
 
 exit 0
