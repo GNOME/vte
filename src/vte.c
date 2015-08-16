e038c223bb438d9e29810ecd659741e9bac14747
Egmont Koblinger <egmont@gmail.com>
2015-08-16 12:31:39 +0200

    widget: Ensure cursor is shown when blinking gets turned off
    https://bugzilla.gnome.org/show_bug.cgi?id=753645
    (cherry picked from commit c1db666000f9c8e3d05930690926a6d7908e8e41)

--- src/vte.c
+++ src/vte.c
@@ -4683,6 +4683,10 @@ remove_cursor_timeout (VteTerminal *terminal)
 
 	g_source_remove (terminal->pvt->cursor_blink_tag);
 	terminal->pvt->cursor_blink_tag = 0;
+        if (terminal->pvt->cursor_blink_state == FALSE) {
+                _vte_invalidate_cursor_once(terminal, FALSE);
+                terminal->pvt->cursor_blink_state = TRUE;
+        }
 }
 
 /* Activates / disactivates the cursor blink timer to reduce wakeups */
@@ -4825,10 +4829,6 @@ vte_terminal_key_press(GtkWidget *widget, GdkEventKey *event)
 		if (terminal->pvt->cursor_blink_tag != 0)
 		{
 			remove_cursor_timeout (terminal);
-                        if (terminal->pvt->cursor_blink_state == FALSE) {
-                                _vte_invalidate_cursor_once(terminal, FALSE);
-                                terminal->pvt->cursor_blink_state = TRUE;
-                        }
 			add_cursor_timeout (terminal);
 		}
 
