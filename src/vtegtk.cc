12d6af2c3a78b6b8c584918a389d31d4a63910f0
Egmont Koblinger <egmont@gmail.com>
2016-07-24 19:06:52 +0200

    widget: Fix a memory leak in set_colors
    https://bugzilla.gnome.org/show_bug.cgi?id=768994
    (cherry picked from commit be0ae73fe58b9a3b37a0ca9568ac5999416552a2)

--- src/vtegtk.cc
+++ src/vtegtk.cc
@@ -2898,6 +2898,7 @@ vte_terminal_set_colors(VteTerminal *terminal,
                          background ? &bg : nullptr,
                          pal, palette_size);
         impl->set_background_alpha(background ? background->alpha : 1.0);
+        g_free(pal);
 }
 
 /**
