GNOME/vte

commit 50aee40af7d16c2bec82f4f8bcdd259b6913c541

widget: Move some methods to VteTerminalPrivate

src/vte.cc
@@ -3367,42 +3367,49 @@ vte_terminal_child_watch_cb(GPid pid,
 		return;
 	}
 
-	if (pid == terminal->pvt->pty_pid) {
-                GObject *object = G_OBJECT(terminal);
+        g_object_ref(terminal);
+        terminal->pvt->child_watch_done(pid, status);
+        g_object_unref(terminal);
+        /* Note: terminal may be destroyed at this point */
+}
 
-                g_object_ref(object);
-                g_object_freeze_notify(object);
+void
+VteTerminalPrivate::child_watch_done(GPid pid,
+                                     int status)
+{
+	if (pid != m_pty_pid)
+                return;
 
-		_VTE_DEBUG_IF (VTE_DEBUG_LIFECYCLE) {
-			g_printerr ("Child[%d] exited with status %d\n",
-					pid, status);
+        GObject *object = G_OBJECT(m_terminal);
+        g_object_ref(object);
+        g_object_freeze_notify(object);
+
+        _VTE_DEBUG_IF (VTE_DEBUG_LIFECYCLE) {
+                g_printerr ("Child[%d] exited with status %d\n",
+                            pid, status);
 #ifdef HAVE_SYS_WAIT_H
-			if (WIFEXITED (status)) {
-				g_printerr ("Child[%d] exit code %d.\n",
-						pid, WEXITSTATUS (status));
-			}else if (WIFSIGNALED (status)) {
-				g_printerr ("Child[%d] dies with signal %d.\n",
-						pid, WTERMSIG (status));
-			}
+                if (WIFEXITED (status)) {
+                        g_printerr ("Child[%d] exit code %d.\n",
+                                    pid, WEXITSTATUS (status));
+                } else if (WIFSIGNALED (status)) {
+                        g_printerr ("Child[%d] dies with signal %d.\n",
+                                    pid, WTERMSIG (status));
+                }
 #endif
-		}
+        }
 
-		terminal->pvt->child_watch_source = 0;
-		terminal->pvt->pty_pid = -1;
+        m_child_watch_source = 0;
+        m_pty_pid = -1;
 
-		/* Close out the PTY. */
-                vte_terminal_set_pty(terminal, NULL);
+        /* Close out the PTY. */
+        set_pty(nullptr);
 
-		/* Tell observers what's happened. */
-                _vte_debug_print(VTE_DEBUG_SIGNALS,
-                                 "Emitting `child-exited'.\n");
-                g_signal_emit(object, signals[SIGNAL_CHILD_EXITED], 0, status);
+        /* Tell observers what's happened. */
+        _vte_debug_print(VTE_DEBUG_SIGNALS,
+                         "Emitting `child-exited'.\n");
+        g_signal_emit(object, signals[SIGNAL_CHILD_EXITED], 0, status);
 
-                g_object_thaw_notify(object);
-                g_object_unref(object);
-
-                /* Note: terminal may be destroyed at this point */
-	}
+        g_object_thaw_notify(object);
 }
 
 static void mark_input_source_invalid(VteTerminal *terminal)

src/vteinternal.hh
@@ -537,6 +537,9 @@ public:
         void pty_scroll_lock_changed(bool locked);
 
         void watch_child (GPid child_pid);
+        void child_watch_done(GPid pid,
+                              int status);
+
         bool spawn_sync(VtePtyFlags pty_flags,
                         const char *working_directory,
                         char **argv,

commit 167f6607a779c9b57c0f856df244aa83aa8f2651

widget: Fix refcount leak in child_watch_done
VteTerminalPrivate::child_watch_done() was missing the g_object_unref()
corresponding to its g_object_ref() at the start.

Since this function is only called from the callback of
g_child_watch_add_full(), and that callback already uses g_object_ref()/
g_object_unref() around the call to VteTerminalPrivate::child_watch_done
to keep the VteTerminalPrivate in a death grip, just remove the extra
g_object_ref().

The bug was introduced in commit 50aee40af7d16c2bec82f4f8bcdd259b6913c541.

https://bugzilla.gnome.org/show_bug.cgi?id=770036

src/vte.cc
@@ -3130,7 +3130,6 @@ VteTerminalPrivate::child_watch_done(GPid pid,
                 return;
 
         GObject *object = G_OBJECT(m_terminal);
-        g_object_ref(object);
         g_object_freeze_notify(object);
 
         _VTE_DEBUG_IF (VTE_DEBUG_LIFECYCLE) {