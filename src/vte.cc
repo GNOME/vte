GNOME/vte

commit d046ae112f5ca93378c2146c1ea239bc63778364

widget: Correct end row in calls to get_text()
We need to pass row+1 if we use -1 as end column to mean
'all the text on the last row'.

src/vte.cc
@@ -11172,7 +11172,7 @@ VteTerminalPrivate::search_rows(
 
 
 	row_text = get_text(start_row, 0,
-                            end_row, -1,
+                            end_row + 1, -1,
                             false /* block */,
                             true /* wrap */,
                             false /* include trailing whitespace */, /* FIXMEchpe maybe do include it since the match may depend on it? */
@@ -11253,7 +11253,7 @@ VteTerminalPrivate::search_rows(
 		m_search_attrs = g_array_new (FALSE, TRUE, sizeof (VteCharAttributes));
 	attrs = m_search_attrs;
 	row_text = get_text(start_row, 0,
-                            end_row, -1,
+                            end_row + 1, -1,
                             false /* block */,
                             true /* wrap */,
                             false /* include trailing whitespace */, /* FIXMEchpe maybe true? */

commit d4d201f80ca1f401e39daab4c8e6bd0836e02696

widget: Fix backward search
Search was getting two lines of text at once, breaking backward search.
Problem was introduced in commit d046ae112f5ca93378c2146c1ea239bc63778364.

https://bugzilla.gnome.org/show_bug.cgi?id=769400

src/vte.cc
@@ -10773,9 +10773,8 @@ VteTerminalPrivate::search_rows(pcre2_match_context_8 *match_context,
 	GArray *attrs;
 	gdouble value, page_size;
 
-
 	row_text = get_text(start_row, 0,
-                            end_row + 1, -1,
+                            end_row, -1,
                             false /* block */,
                             true /* wrap */,
                             false /* include trailing whitespace */, /* FIXMEchpe maybe do include it since the match may depend on it? */
@@ -10823,7 +10822,7 @@ VteTerminalPrivate::search_rows(pcre2_match_context_8 *match_context,
 		m_search_attrs = g_array_new (FALSE, TRUE, sizeof (VteCharAttributes));
 	attrs = m_search_attrs;
 	row_text = get_text(start_row, 0,
-                            end_row + 1, -1,
+                            end_row, -1,
                             false /* block */,
                             true /* wrap */,
                             false /* include trailing whitespace */, /* FIXMEchpe maybe true? */