6301abbbbe3f1293952fdd62f9abcbb2a0dc4ae8
Christian Persch <chpe@src.gnome.org>
2016-09-17 20:33:47 +0200

    widget: Fix backward search
    Search was getting two lines of text at once, breaking backward search.
    Problem was introduced in commit d046ae112f5ca93378c2146c1ea239bc63778364.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=769400
    (cherry picked from commit d4d201f80ca1f401e39daab4c8e6bd0836e02696)

--- src/vte.cc
+++ src/vte.cc
@@ -11050,9 +11050,8 @@ VteTerminalPrivate::search_rows(
 	GArray *attrs;
 	gdouble value, page_size;
 
-
 	row_text = get_text(start_row, 0,
-                            end_row + 1, -1,
+                            end_row, -1,
                             false /* block */,
                             true /* wrap */,
                             false /* include trailing whitespace */, /* FIXMEchpe maybe do include it since the match may depend on it? */
@@ -11133,7 +11132,7 @@ VteTerminalPrivate::search_rows(
 		m_search_attrs = g_array_new (FALSE, TRUE, sizeof (VteCharAttributes));
 	attrs = m_search_attrs;
 	row_text = get_text(start_row, 0,
-                            end_row + 1, -1,
+                            end_row, -1,
                             false /* block */,
                             true /* wrap */,
                             false /* include trailing whitespace */, /* FIXMEchpe maybe true? */
