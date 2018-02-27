2e5dfbd89b96e90509e4d738f0e18f4ea163084c
Egmont Koblinger <egmont@gmail.com>
2018-02-27 22:29:04 +0100

    widget: Unset hyperlink URI when moving the mouse to a non-hyperlink cell
    https://bugzilla.gnome.org/show_bug.cgi?id=789536

--- src/vte.cc
+++ src/vte.cc
@@ -5637,10 +5637,12 @@ VteTerminalPrivate::hyperlink_hilite_update(vte::view::coords const& pos)
         /* This might be different from new_hyperlink_hover_idx. If in the stream, that one contains
          * the pseudo idx VTE_HYPERLINK_IDX_TARGET_IN_STREAM and now a real idx is allocated.
          * Plus, the ring's internal belief of the hovered hyperlink is also updated. */
-        if (coords_visible)
+        if (coords_visible) {
                 m_hyperlink_hover_idx = _vte_ring_get_hyperlink_at_position(m_screen->row_data, rowcol.row(), rowcol.column(), true, &m_hyperlink_hover_uri);
-        else
+        } else {
                 m_hyperlink_hover_idx = 0;
+                m_hyperlink_hover_uri = nullptr;
+        }
 
         /* Invalidate cells of the new hyperlink. Get the bounding box. */
         if (m_hyperlink_hover_idx != 0) {
