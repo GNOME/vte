fd1956fb96e229d4176cc118274f325696f46e8e
Christian Persch <chpe@src.gnome.org>
2018-04-23 20:51:18 +0200

    parser: Update copyright notice

--- src/parser.hh
+++ src/parser.hh
@@ -1,5 +1,6 @@
 /*
  * Copyright © 2015 David Herrmann <dh.herrmann@gmail.com>
+ * Copyright © 2018 Christian Persch
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
