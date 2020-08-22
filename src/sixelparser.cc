97d2e87fa196e4023bf658bc56701cac27063434
Christian Persch <chpe@src.gnome.org>
2020-08-22 20:53:10 +0200

    all: Fix sixelparser.{hh,cc} licence header

--- src/sixelparser.cc
+++ src/sixelparser.cc
@@ -4,14 +4,14 @@
  * originally written by kmiya@cluti (https://github.com/saitoha/sixel/blob/master/fromsixel.c)
  *
  * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
+ * modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
