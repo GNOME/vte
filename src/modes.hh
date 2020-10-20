GNOME/vte

commit ce715e7b9859f62fc881c2ddd64cab7e3a2fbc7a

modes: Add DECSDM mode

src/modes.hh
@@ -226,6 +226,7 @@ public:
 
         constexpr Private() : Self{eDEC_AUTOWRAP,
                                    eDEC_TEXT_CURSOR,
+                                   eDECSDM,
                                    eVTE_BIDI_SWAP_ARROW_KEYS,
                                    eXTERM_ALTBUF_SCROLL,
                                    eXTERM_META_SENDS_ESCAPE} { }

src/modes-private.hh
@@ -415,15 +415,13 @@ MODE_FIXED(DECLRMM, 69, ALWAYS_RESET) /* aka DECVSSM */
 MODE_FIXED(DECXRLM,    73, ALWAYS_RESET)
 
 /*
- * DECSDM - sixel display mode
+ * DECSDM - sixel display mode (scrolling)
  *
- * Default: reset
+ * Default: set
  *
- * References: VT525
- *
- * Probably not worth implementing.
+ * References: ?
  */
-/* MODE_FIXED(DECSDM,    80, ALWAYS_RESET) ! Conflicts with WY161 */
+MODE(DECSDM, 80) /* Note: Conflicts with WY161 */
 
 /*
  * DECKPM - key position mode
@@ -799,7 +797,7 @@ MODE_FIXED(WYTEK,  38, ALWAYS_RESET)
  *
  * References: WY370
  */
-MODE_FIXED(WY161,  80, ALWAYS_RESET)
+/* MODE_FIXED(WY161,  80, ALWAYS_RESET) ! Conflicts with DECSDM */
 
 /*
  * WY52 - 52 lines mode

commit 0afc1c1b9fa739251c04b54f751c0f9f3f762294

modes: Assign enum values to non-fixed modes first
This allows all settable modes to have values that fit into the storage,
while fixed modes get assigned enum values after all settable modes.

Fixes a regression introduced by commit ce715e7b9859f62fc881c2ddd64cab7e3a2fbc7a.

src/modes.hh
@@ -129,6 +129,11 @@ public:
                 eALWAYS_RESET = -1,
 
 #define MODE(name,param) e##name,
+#define MODE_FIXED(name,param,value)
+#include "modes-ecma.hh"
+#undef MODE
+#undef MODE_FIXED
+#define MODE(name,param)
 #define MODE_FIXED(name,param,value) e##name,
 #include "modes-ecma.hh"
 #undef MODE
@@ -183,6 +188,11 @@ public:
                 eALWAYS_RESET = -1,
 
 #define MODE(name,param) e##name,
+#define MODE_FIXED(name,param,value)
+#include "modes-private.hh"
+#undef MODE
+#undef MODE_FIXED
+#define MODE(name,param)
 #define MODE_FIXED(name,param,value) e##name,
 #include "modes-private.hh"
 #undef MODE