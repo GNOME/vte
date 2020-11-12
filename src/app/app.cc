GNOME/vte

commit eb85292b3ef44ac95d3863af68b19f6a8d8131e2

widget: Add accessor for the background colour
https://gitlab.gnome.org/GNOME/vte/issues/9

src/app/app.cc
@@ -331,7 +331,7 @@ public:
                         { "backdrop", 0, 0,G_OPTION_ARG_NONE, &backdrop,
                           "Dim when toplevel unfocused", nullptr },
                         { "background-color", 0, 0, G_OPTION_ARG_CALLBACK, (void*)parse_bg_color,
-                          "Set default background color", nullptr },
+                          "Set default background color", "COLOR" },
                         { "background-image", 0, 0, G_OPTION_ARG_CALLBACK, (void*)parse_background_image,
                           "Set background image from file", "FILE" },
                         { "background-extend", 0, 0, G_OPTION_ARG_CALLBACK, (void*)parse_background_extend,
@@ -347,9 +347,9 @@ public:
                         { "cursor-blink", 0, 0, G_OPTION_ARG_CALLBACK, (void*)parse_cursor_blink,
                           "Cursor blink mode (system|on|off)", "MODE" },
                         { "cursor-background-color", 0, 0, G_OPTION_ARG_CALLBACK, (void*)parse_cursor_bg_color,
-                          "Enable a colored cursor background", nullptr },
+                          "Enable a colored cursor background", "COLOR" },
                         { "cursor-foreground-color", 0, 0, G_OPTION_ARG_CALLBACK, (void*)parse_cursor_fg_color,
-                          "Enable a colored cursor foreground", nullptr },
+                          "Enable a colored cursor foreground", "COLOR" },
                         { "cursor-shape", 0, 0, G_OPTION_ARG_CALLBACK, (void*)parse_cursor_shape,
                           "Set cursor shape (block|underline|ibeam)", nullptr },
                         { "dingu", 'D', 0, G_OPTION_ARG_STRING_ARRAY, &dingus,
@@ -365,15 +365,15 @@ public:
                         { "font", 'f', 0, G_OPTION_ARG_STRING, &font_string,
                           "Specify a font to use", nullptr },
                         { "foreground-color", 0, 0, G_OPTION_ARG_CALLBACK, (void*)parse_fg_color,
-                          "Set default foreground color", nullptr },
+                          "Set default foreground color", "COLOR" },
                         { "gregex", 0, 0, G_OPTION_ARG_NONE, &use_gregex,
                           "Use GRegex instead of PCRE2", nullptr },
                         { "geometry", 'g', 0, G_OPTION_ARG_STRING, &geometry,
                           "Set the size (in characters) and position", "GEOMETRY" },
                         { "highlight-background-color", 0, 0, G_OPTION_ARG_CALLBACK, (void*)parse_hl_bg_color,
-                          "Enable distinct highlight background color for selection", nullptr },
+                          "Enable distinct highlight background color for selection", "COLOR" },
                         { "highlight-foreground-color", 0, 0, G_OPTION_ARG_CALLBACK, (void*)parse_hl_fg_color,
-                          "Enable distinct highlight foreground color for selection", nullptr },
+                          "Enable distinct highlight foreground color for selection", "COLOR" },
                         { "icon-title", 'i', 0, G_OPTION_ARG_NONE, &icon_title,
                           "Enable the setting of the icon title", nullptr },
                         { "keep", 'k', 0, G_OPTION_ARG_NONE, &keep,
@@ -873,7 +873,8 @@ vteapp_terminal_draw(GtkWidget* widget,
                 cairo_rectangle(cr, 0.0, 0.0,
                                 gtk_widget_get_allocated_width(widget),
                                 gtk_widget_get_allocated_height(widget));
-                auto bg = options.get_color_bg();
+                GdkRGBA bg;
+                vte_terminal_get_color_background_for_draw(VTE_TERMINAL(terminal), &bg);
                 cairo_set_source_rgba(cr, bg.red, bg.green, bg.blue, 1.0);
                 cairo_paint(cr);
 
@@ -883,7 +884,7 @@ vteapp_terminal_draw(GtkWidget* widget,
                 cairo_paint(cr);
 
                 cairo_pop_group_to_source(cr);
-                cairo_paint_with_alpha(cr, options.get_alpha());
+                cairo_paint_with_alpha(cr, bg.alpha);
 
         }
 

doc/reference/vte-docs.xml
@@ -110,6 +110,10 @@
     <title>Index of new symbols in 0.52</title>
     <xi:include href="xml/api-index-0.52.xml"><xi:fallback /></xi:include>
   </index>
+  <index id="api-index-0-54" role="0.54">
+    <title>Index of new symbols in 0.54</title>
+    <xi:include href="xml/api-index-0.54.xml"><xi:fallback /></xi:include>
+  </index>
 
   <xi:include href="xml/annotation-glossary.xml"><xi:fallback /></xi:include>
 

src/vte/vteterminal.h
@@ -471,6 +471,9 @@ gboolean vte_terminal_get_input_enabled (VteTerminal *terminal) _VTE_GNUC_NONNUL
 _VTE_PUBLIC
 void vte_terminal_set_clear_background(VteTerminal* terminal,
                                        gboolean setting) _VTE_GNUC_NONNULL(1);
+_VTE_PUBLIC
+void vte_terminal_get_color_background_for_draw(VteTerminal* terminal,
+                                                GdkRGBA* color) _VTE_GNUC_NONNULL(1) _VTE_GNUC_NONNULL(2);
 
 /* Writing contents out */
 _VTE_PUBLIC

src/vtegtk.cc
@@ -4291,3 +4291,36 @@ vte_terminal_set_clear_background(VteTerminal* terminal,
 
         IMPL(terminal)->set_clear_background(setting != FALSE);
 }
+
+/**
+ * vte_terminal_get_color_background_for_draw:
+ * @terminal: a #VteTerminal
+ * @color: (out): a location to store a #GdbRGBA color
+ *
+ * Returns the background colour, as used by @terminal when
+ * drawing the background, which may be different from
+ * the color set by vte_terminal_set_color_background().
+ *
+ * Note: you must only call this function while handling the
+ * GtkWidget::draw signal.
+ *
+ * This function is rarely useful. One use for it is if you disable
+ * drawing the background (see vte_terminal_set_clear_background())
+ * and then need to draw the background yourself.
+ *
+ * Since: 0.54
+ */
+void
+vte_terminal_get_color_background_for_draw(VteTerminal* terminal,
+                                           GdkRGBA* color)
+{
+        g_return_if_fail(VTE_IS_TERMINAL(terminal));
+        g_return_if_fail(color != nullptr);
+
+        auto impl = IMPL(terminal);
+        auto const c = impl->get_color(VTE_DEFAULT_BG);
+        color->red = c->red / 65535.;
+        color->green = c->green / 65535.;
+        color->blue = c->blue / 65535.;
+        color->alpha = impl->m_background_alpha;
+}

commit 79e40dd5387a6a54ce418a082a4ebb6eaf60b20e

app: Fix background image drawing
Use the option's background alpha value, *not* the value retrieved from
vte_terminal_get_color_background_for_draw() since that is set to zero
in this case.

Fixes a regression from commit eb85292b3ef44ac95d3863af68b19f6a8d8131e2.

Fixes: https://gitlab.gnome.org/GNOME/vte/-/issues/301

src/app/app.cc
@@ -454,6 +454,17 @@ public:
                 return alpha;
         }
 
+        double get_alpha_bg_for_draw() const
+        {
+                double alpha;
+                if (whole_window_transparent)
+                        alpha = 1.0;
+                else
+                        alpha = get_alpha();
+
+                return alpha;
+        }
+
         GdkRGBA get_color_bg() const
         {
                 GdkRGBA color{bg_color};
@@ -1041,7 +1052,7 @@ vteapp_terminal_draw(GtkWidget* widget,
                 cairo_paint(cr);
 
                 cairo_pop_group_to_source(cr);
-                cairo_paint_with_alpha(cr, bg.alpha);
+                cairo_paint_with_alpha(cr, options.get_alpha_bg_for_draw());
 
         }
 