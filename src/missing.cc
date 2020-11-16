efebf38e278b80ef4670dad87d749898cf244f88
Matt Rose <mattrose@folkwolf.net>
2020-11-16 20:38:04 +0100

    lib: Use sysconf on macos to get the upper limit of number of FDs
    Fixes: https://gitlab.gnome.org/GNOME/vte/-/issues/296

--- src/missing.cc
+++ src/missing.cc
@@ -113,7 +113,7 @@ getrlimit_NOFILE_max(void)
 
 #endif /* HAVE_SYS_RESOURCE_H */
 
-#if defined(__FreeBSD__) || defined(__OpenBSD__)
+#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__APPLE__)
         /* Use sysconf() function provided by the system if it is known to be
          * async-signal safe.
          */
