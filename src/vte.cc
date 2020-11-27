GNOME/vte

commit 868602fdbe590761418f6be1242b625d494078b1

lib: Sanitise the passed-in font description
Remove weight and style if set, so that SGR attributes can work.

(cherry picked from commit 11cb693c816b241e8660885dc48f2f5a1c6047ff)

src/vte.cc
@@ -7327,6 +7327,14 @@ Terminal::set_font_desc(PangoFontDescription const* font_desc)
 				"Using default monospace font.\n");
 	}
 
+        /* Sanitise the  font description. Style and weight need to be default here,
+         * since those are set via SGR attributes; and gravity makes no sense in vte.
+         */
+        pango_font_description_unset_fields(desc.get(),
+                                            PangoFontMask(PANGO_FONT_MASK_GRAVITY |
+                                                          PANGO_FONT_MASK_STYLE |
+                                                          PANGO_FONT_MASK_WEIGHT));
+
         bool const same_desc = m_unscaled_font_desc &&
                 pango_font_description_equal(m_unscaled_font_desc.get(), desc);
 

commit e9d8d9bd9486d6fb84e010cfed7bc6dcac5354c2

lib: desc is not a smart pointer in 0.62
Fixes a regression from 868602fdbe590761418f6be1242b625d494078b1.

Fixes: https://gitlab.gnome.org/GNOME/vte/-/issues/314

src/vte.cc
@@ -7330,7 +7330,7 @@ Terminal::set_font_desc(PangoFontDescription const* font_desc)
         /* Sanitise the  font description. Style and weight need to be default here,
          * since those are set via SGR attributes; and gravity makes no sense in vte.
          */
-        pango_font_description_unset_fields(desc.get(),
+        pango_font_description_unset_fields(desc,
                                             PangoFontMask(PANGO_FONT_MASK_GRAVITY |
                                                           PANGO_FONT_MASK_STYLE |
                                                           PANGO_FONT_MASK_WEIGHT));

src/vtegtk.cc
@@ -2095,7 +2095,6 @@ vte_get_features (void) noexcept
 #endif
                 " "
 #ifdef __linux__
-                " "
 #ifdef WITH_SYSTEMD
                 "+SYSTEMD"
 #else