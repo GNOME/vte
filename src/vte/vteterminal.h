650567f48f4f4338993206682ea0e9369b1395d7
Christian Persch <chpe@src.gnome.org>
2021-02-05 22:43:03 +0100

    lib: Fix indentation

--- src/vte/vteterminal.h
+++ src/vte/vteterminal.h
@@ -258,7 +258,7 @@ gboolean vte_terminal_get_scroll_on_keystroke(VteTerminal *terminal) _VTE_CXX_NO
 
 _VTE_PUBLIC
 void vte_terminal_set_enable_fallback_scrolling(VteTerminal *terminal,
-                                         gboolean enable) _VTE_CXX_NOEXCEPT _VTE_GNUC_NONNULL(1);
+                                                gboolean enable) _VTE_CXX_NOEXCEPT _VTE_GNUC_NONNULL(1);
 _VTE_PUBLIC
 gboolean vte_terminal_get_enable_fallback_scrolling(VteTerminal *terminal)  _VTE_CXX_NOEXCEPT _VTE_GNUC_NONNULL(1);
 
