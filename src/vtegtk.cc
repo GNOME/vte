d578bd30b18a0d040305f356a3327fbd011e5451
Christian Persch <chpe@src.gnome.org>
2021-02-13 22:52:05 +0100

    all: Remove SIXEL support from stable branch
    The SIXEL support is not in a releasable state with
    important and fundamental problems still unsolved.

--- src/vtegtk.cc
+++ src/vtegtk.cc
@@ -1844,11 +1844,7 @@ vte_terminal_class_init(VteTerminalClass *klass)
          */
         pspecs[PROP_ENABLE_SIXEL] =
                 g_param_spec_boolean ("enable-sixel", nullptr, nullptr,
-#ifdef WITH_SIXEL
-                                      VTE_SIXEL_ENABLED_DEFAULT,
-#else
                                       false,
-#endif
                                       (GParamFlags) (G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS | G_PARAM_EXPLICIT_NOTIFY));
 
 
@@ -2161,12 +2157,6 @@ vte_get_features (void) noexcept
 #else
                 "-ICU"
 #endif
-                " "
-#ifdef WITH_SIXEL
-                "+SIXEL"
-#else
-                "-SIXEL"
-#endif
 #ifdef __linux__
                 " "
 #ifdef WITH_SYSTEMD
@@ -2196,9 +2186,6 @@ vte_get_feature_flags(void) noexcept
 #endif
 #ifdef WITH_ICU
                                VTE_FEATURE_FLAG_ICU |
-#endif
-#ifdef WITH_SIXEL
-                               VTE_FEATURE_FLAG_SIXEL |
 #endif
 #ifdef __linux__
 #ifdef WITH_SYSTEMD
@@ -5778,12 +5765,7 @@ vte_terminal_set_enable_sixel(VteTerminal *terminal,
                               gboolean enabled) noexcept
 try
 {
-#ifdef WITH_SIXEL
         g_return_if_fail(VTE_IS_TERMINAL(terminal));
-
-        if (WIDGET(terminal)->set_sixel_enabled(enabled != FALSE))
-                g_object_notify_by_pspec(G_OBJECT(terminal), pspecs[PROP_ENABLE_SIXEL]);
-#endif
 }
 catch (...)
 {
@@ -5794,7 +5776,7 @@ catch (...)
  * vte_terminal_get_enable_sixel:
  * @terminal: a #VteTerminal
  *
- * Returns: %TRUE if SIXEL image support is enabled, %FALSE otherwise
+ * Returns: %FALSE
  *
  * Since: 0.62
  */
@@ -5802,13 +5784,7 @@ gboolean
 vte_terminal_get_enable_sixel(VteTerminal *terminal) noexcept
 try
 {
-#ifdef WITH_SIXEL
-        g_return_val_if_fail(VTE_IS_TERMINAL(terminal), FALSE);
-
-        return WIDGET(terminal)->sixel_enabled();
-#else
         return false;
-#endif
 }
 catch (...)
 {
