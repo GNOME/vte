b7ae82fbbe61d313f2fe945957a9d8443e231af2
Christian Persch <chpe@src.gnome.org>
2022-04-07 16:44:09 +0200

    minifont: Implement separated block quadrants

--- src/cairo-glue.hh
+++ src/cairo-glue.hh
@@ -23,6 +23,7 @@
 
 namespace vte {
 
+VTE_DECLARE_FREEABLE(cairo_pattern_t, cairo_pattern_destroy);
 VTE_DECLARE_FREEABLE(cairo_rectangle_list_t, cairo_rectangle_list_destroy);
 VTE_DECLARE_FREEABLE(cairo_region_t, cairo_region_destroy);
 VTE_DECLARE_FREEABLE(cairo_surface_t, cairo_surface_destroy);
