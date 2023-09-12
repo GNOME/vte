GNOME/vte

commit fb3220bee959d37f58daf702165f142ef9fab9f4

all: Use defined values for defines
... instead of defined/undefined names, so that -Wundef can catch
mistakes.

src/vte.cc
@@ -78,11 +78,9 @@
 #include "cxx-utils.hh"
 #include "gobject-glue.hh"
 
-#ifdef WITH_A11Y
+#if WITH_A11Y
 #if VTE_GTK == 3
 #include "vteaccess.h"
-#else
-#undef WITH_A11Y
 #endif /* VTE_GTK == 3 */
 #endif /* WITH_A11Y */
 
@@ -90,7 +88,7 @@
 
 using namespace std::literals;
 
-#ifndef HAVE_ROUND
+#if !HAVE_ROUND
 static inline double round(double x) {
 	if(x - floor(x) < 0.5) {
 		return floor(x);
@@ -98,7 +96,7 @@ static inline double round(double x) {
 		return ceil(x);
 	}
 }
-#endif
+#endif /* !HAVE_ROUND */
 
 #define WORD_CHAR_EXCEPTIONS_DEFAULT "-#%&+,./=?@\\_~\302\267"sv
 
@@ -274,7 +272,7 @@ vte_g_array_fill(GArray *array, gconstpointer item, guint final_size)
 void
 Terminal::unset_widget() noexcept
 {
-#ifdef WITH_A11Y
+#if WITH_A11Y && VTE_GTK == 3
         set_accessible(nullptr);
 #endif
 
@@ -2146,7 +2144,7 @@ Terminal::set_encoding(char const* charset,
         auto const to_utf8 = bool{charset == nullptr || g_ascii_strcasecmp(charset, "UTF-8") == 0};
         auto const primary_is_current = (current_data_syntax() == primary_data_syntax());
 
-#ifdef WITH_ICU
+#if WITH_ICU
         /* Note that if the current data syntax is not a primary one, the change
          * will only be applied when returning to the primrary data syntax.
          */
@@ -3149,7 +3147,7 @@ not_inserted:
         m_line_wrapped = line_wrapped;
 }
 
-#ifdef WITH_SIXEL
+#if WITH_SIXEL
 
 void
 Terminal::insert_image(ProcessingContext& context,
@@ -3543,13 +3541,13 @@ Terminal::process_incoming()
                         process_incoming_utf8(context, *chunk);
                         break;
 
-#ifdef WITH_ICU
+#if WITH_ICU
                 case DataSyntax::ECMA48_PCTERM:
                         process_incoming_pcterm(context, *chunk);
                         break;
 #endif
 
-#ifdef WITH_SIXEL
+#if WITH_SIXEL
                 case DataSyntax::DECSIXEL:
                         process_incoming_decsixel(context, *chunk);
                         break;
@@ -3571,7 +3569,7 @@ Terminal::process_incoming()
                         m_incoming_queue.pop();
         }
 
-#ifdef VTE_DEBUG
+#if VTE_DEBUG
         /* Some safety checks: ensure the visible parts of the buffer
          * are all in the buffer. */
         g_assert_cmpint(m_screen->insert_delta, >=, _vte_ring_delta(m_screen->row_data));
@@ -3672,18 +3670,18 @@ Terminal::process_incoming_utf8(ProcessingContext& context,
                 case vte::base::UTF8Decoder::ACCEPT: {
                         auto rv = m_parser.feed(m_utf8_decoder.codepoint());
                         if (G_UNLIKELY(rv < 0)) {
-#ifdef DEBUG
+#if VTE_DEBUG
                                 uint32_t c = m_utf8_decoder.codepoint();
                                 char c_buf[7];
                                 g_snprintf(c_buf, sizeof(c_buf), "%lc", c);
                                 char const* wp_str = g_unichar_isprint(c) ? c_buf : _vte_debug_sequence_to_string(c_buf, -1);
                                 _vte_debug_print(VTE_DEBUG_PARSER, "Parser error on U+%04X [%s]!\n",
                                                  c, wp_str);
-#endif
+#endif /* VTE_DEBUG */
                                 break;
                         }
 
-#ifdef VTE_DEBUG
+#if VTE_DEBUG
                         if (rv != VTE_SEQ_NONE)
                                 g_assert((bool)seq);
 #endif
@@ -3764,7 +3762,7 @@ switched_data_syntax:
         chunk.set_begin_reading(ip);
 }
 
-#ifdef WITH_ICU
+#if WITH_ICU
 
 /* Note that this is mostly a copy of process_incoming_utf8() above; any non-charset-decoding
  * related changes made here need to be made there, too.
@@ -3789,7 +3787,7 @@ Terminal::process_incoming_pcterm(ProcessingContext& context,
                 case vte::base::ICUDecoder::Result::eSomething: {
                         auto rv = m_parser.feed(decoder.codepoint());
                         if (G_UNLIKELY(rv < 0)) {
-#ifdef VTE_DEBUG
+#if VTE_DEBUG
                                 uint32_t c = decoder.codepoint();
                                 char c_buf[7];
                                 g_snprintf(c_buf, sizeof(c_buf), "%lc", c);
@@ -3800,7 +3798,7 @@ Terminal::process_incoming_pcterm(ProcessingContext& context,
                                 break;
                         }
 
-#ifdef VTE_DEBUG
+#if VTE_DEBUG
                         if (rv != VTE_SEQ_NONE)
                                 g_assert((bool)seq);
 #endif
@@ -3897,7 +3895,7 @@ Terminal::process_incoming_pcterm(ProcessingContext& context,
 
 #endif /* WITH_ICU */
 
-#ifdef WITH_SIXEL
+#if WITH_SIXEL
 
 void
 Terminal::process_incoming_decsixel(ProcessingContext& context,
@@ -4272,7 +4270,7 @@ Terminal::send_child(std::string_view const& data)
                         _vte_byte_array_append(m_outgoing, data.data(), data.size());
                 break;
 
-#ifdef WITH_ICU
+#if WITH_ICU
         case DataSyntax::ECMA48_PCTERM: {
                 auto converted = m_converter->convert(data);
 
@@ -6285,7 +6283,7 @@ Terminal::get_selected_text(GArray *attributes)
                         attributes);
 }
 
-#ifdef VTE_DEBUG
+#if VTE_DEBUG
 unsigned int
 Terminal::checksum_area(vte::grid::row_t start_row,
                                   vte::grid::column_t start_col,
@@ -7773,7 +7771,7 @@ Terminal::Terminal(vte::platform::Widget* w,
 
         update_view_extents();
 
-#ifdef VTE_DEBUG
+#if VTE_DEBUG
         if (g_test_flags != 0) {
                 feed("\e[1m\e[31mWARNING:\e[39m Test mode enabled. This is insecure!\e[0m\n\e[G"sv, false);
         }
@@ -8302,7 +8300,7 @@ Terminal::draw_cells(vte::view::DrawingContext::TextRequest* items,
                                 }
                         }
 
-#ifdef WITH_SIXEL
+#if WITH_SIXEL
                         if (back == VTE_DEFAULT_BG) {
                                 /* Clear cells in order to properly overdraw images */
                                 m_draw.clear(xl,
@@ -9299,7 +9297,7 @@ Terminal::paint_im_preedit_string()
 void
 Terminal::widget_draw(cairo_t* cr) noexcept
 {
-#ifdef VTE_DEBUG
+#if VTE_DEBUG
         _VTE_DEBUG_IF(VTE_DEBUG_LIFECYCLE | VTE_DEBUG_WORK | VTE_DEBUG_UPDATES) do {
                 auto clip_rect = cairo_rectangle_int_t{};
                 if (!gdk_cairo_get_clip_rectangle (cr, &clip_rect))
@@ -9377,7 +9375,7 @@ Terminal::draw(cairo_t* cr,
         int allocated_width, allocated_height;
         int extra_area_for_cursor;
         bool text_blink_enabled_now;
-#ifdef WITH_SIXEL
+#if WITH_SIXEL
         VteRing *ring = m_screen->row_data;
 #endif
         gint64 now = 0;
@@ -9422,7 +9420,7 @@ Terminal::draw(cairo_t* cr,
                         allocated_height - m_border.top - m_border.bottom);
         cairo_clip(cr);
 
-#ifdef WITH_SIXEL
+#if WITH_SIXEL
 	/* Draw images */
 	if (m_images_enabled) {
 		vte::grid::row_t top_row = first_displayed_row();
@@ -9995,7 +9993,7 @@ Terminal::reset_decoder()
                 m_utf8_decoder.reset();
                 break;
 
-#ifdef WITH_ICU
+#if WITH_ICU
         case DataSyntax::ECMA48_PCTERM:
                 m_converter->decoder().reset();
                 break;
@@ -10013,7 +10011,7 @@ Terminal::reset_data_syntax()
                 return;
 
         switch (current_data_syntax()) {
-#ifdef WITH_SIXEL
+#if WITH_SIXEL
         case DataSyntax::DECSIXEL:
                 m_sixel_context->reset();
                 break;
@@ -10029,7 +10027,7 @@ Terminal::reset_data_syntax()
 void
 Terminal::reset_graphics_color_registers()
 {
-#ifdef WITH_SIXEL
+#if WITH_SIXEL
         if (m_sixel_context)
                 m_sixel_context->reset_colors();
 #endif
@@ -10139,7 +10137,7 @@ Terminal::reset(bool clear_tabstops,
 	/* Clear modifiers. */
 	m_modifiers = 0;
 
-#ifdef WITH_SIXEL
+#if WITH_SIXEL
         if (m_sixel_context)
                 m_sixel_context->reset_colors();
 #endif

meson.build
@@ -182,12 +182,12 @@ config_h = configuration_data()
 
 config_h.set_quoted('GETTEXT_PACKAGE', vte_gettext_domain)
 config_h.set_quoted('VERSION', vte_version)
-config_h.set('VTE_DEBUG', enable_debug)
-config_h.set('WITH_A11Y', get_option('a11y'))
-config_h.set('WITH_FRIBIDI', get_option('fribidi'))
-config_h.set('WITH_GNUTLS', get_option('gnutls'))
-config_h.set('WITH_ICU', get_option('icu'))
-config_h.set('WITH_SIXEL', get_option('sixel'))
+config_h.set10('VTE_DEBUG', enable_debug)
+config_h.set10('WITH_A11Y', get_option('a11y'))
+config_h.set10('WITH_FRIBIDI', get_option('fribidi'))
+config_h.set10('WITH_GNUTLS', get_option('gnutls'))
+config_h.set10('WITH_ICU', get_option('icu'))
+config_h.set10('WITH_SIXEL', get_option('sixel'))
 
 ver = glib_min_req_version.split('.')
 config_h.set('GLIB_VERSION_MIN_REQUIRED', '(G_ENCODE_VERSION(' + ver[0] + ',' + ver[1] + '))')
@@ -423,7 +423,7 @@ foreach func: check_functions
   if __reqd
     assert(__have, __name + ' not found')
   else
-    config_h.set('HAVE_' + __name.underscorify().to_upper(), __have)
+    config_h.set10('HAVE_' + __name.underscorify().to_upper(), __have)
   endif
 endforeach
 
@@ -644,7 +644,7 @@ else
   systemd_dep = dependency('', required: false)
 endif
 
-config_h.set('WITH_SYSTEMD', systemd_dep.found())
+config_h.set10('WITH_SYSTEMD', systemd_dep.found())
 
 # Write config.h
 

src/app/app.cc
@@ -766,7 +766,7 @@ public:
                           "Use a GtkScrolledWindow", nullptr },
                         { "shell", 'S', G_OPTION_FLAG_REVERSE | G_OPTION_FLAG_HIDDEN,
                           G_OPTION_ARG_NONE, &no_shell, nullptr, nullptr },
-#ifdef VTE_DEBUG
+#if VTE_DEBUG
                         { "test-mode", 0, 0, G_OPTION_ARG_NONE, &test_mode,
                           "Enable test mode", nullptr },
 #endif
@@ -3159,7 +3159,7 @@ main(int argc,
                gdk_window_set_debug_updates(true);
 #endif /* VTE_GTK == 3 */
 
-#ifdef VTE_DEBUG
+#if VTE_DEBUG
        if (options.test_mode) {
                vte_set_test_flags(VTE_TEST_FLAGS_ALL);
                options.allow_window_ops = true;

src/bidi.cc
@@ -42,7 +42,7 @@
 
 #include <config.h>
 
-#ifdef WITH_FRIBIDI
+#if WITH_FRIBIDI
 #include <fribidi.h>
 #endif
 
@@ -51,7 +51,7 @@
 #include "vtedefines.hh"
 #include "vteinternal.hh"
 
-#ifdef WITH_FRIBIDI
+#if WITH_FRIBIDI
 static_assert (sizeof (FriBidiChar) == sizeof (gunichar), "Unexpected FriBidiChar size");
 #endif
 
@@ -178,7 +178,7 @@ BidiRow::vis_get_shaped_char(vte::grid::column_t col, vteunistr s) const
 }
 
 
-#ifdef WITH_FRIBIDI
+#if WITH_FRIBIDI
 static inline bool
 is_arabic(gunichar c)
 {
@@ -349,7 +349,7 @@ BidiRunner::explicit_line(vte::grid::row_t row, bool rtl, bool do_shaping)
                 }
         }
 
-#ifdef WITH_FRIBIDI
+#if WITH_FRIBIDI
         if (do_shaping)
                 explicit_line_shape(row);
 #endif
@@ -374,7 +374,7 @@ BidiRunner::paragraph(vte::grid::row_t start, vte::grid::row_t end,
                 return;
         }
 
-#ifdef WITH_FRIBIDI
+#if WITH_FRIBIDI
         /* Have a consistent limit on the number of rows in a paragraph
          * that can get implicit BiDi treatment, which is independent from
          * the current scroll position. */
@@ -399,7 +399,7 @@ BidiRunner::explicit_paragraph(vte::grid::row_t start, vte::grid::row_t end,
         }
 }
 
-#ifdef WITH_FRIBIDI
+#if WITH_FRIBIDI
 /* Figure out the mapping for the implicit paragraph between the given rows.
  * Returns success. */
 bool
@@ -766,7 +766,7 @@ vte_bidi_get_mirror_char (vteunistr unistr, gboolean mirror_box_drawing, vteunis
                 if (G_UNLIKELY (mirror_box_drawing))
                         base_ch_mirrored = 0x2500 + mirrored_2500[base_ch - 0x2500];
         } else {
-#ifdef WITH_FRIBIDI
+#if WITH_FRIBIDI
                 /* Prefer the FriBidi variant as that's more likely to be in sync with the rest of our BiDi stuff. */
                 fribidi_get_mirror_char (base_ch, &base_ch_mirrored);
 #else

src/bidi.hh
@@ -98,13 +98,13 @@ public:
 private:
         RingView *m_ringview;
 
-#ifdef WITH_FRIBIDI
+#if WITH_FRIBIDI
         void explicit_line_shape(vte::grid::row_t row);
 #endif
 
         void explicit_line(vte::grid::row_t row, bool rtl, bool do_shaping);
         void explicit_paragraph(vte::grid::row_t start, vte::grid::row_t end, bool rtl, bool do_shaping);
-#ifdef WITH_FRIBIDI
+#if WITH_FRIBIDI
         bool implicit_paragraph(vte::grid::row_t start, vte::grid::row_t end, bool do_shaping);
 #endif
 };

src/cxx-utils.hh
@@ -46,7 +46,7 @@ inline constexpr auto to_integral(E e) noexcept
         return static_cast<std::underlying_type_t<E>>(e);
 }
 
-#ifdef VTE_DEBUG
+#if VTE_DEBUG
 void log_exception(char const* func = __builtin_FUNCTION(),
                    char const* filename = __builtin_FILE(),
                    int const line = __builtin_LINE()) noexcept;

src/debug.cc
@@ -27,7 +27,7 @@ guint _vte_debug_flags;
 void
 _vte_debug_init(void)
 {
-#ifdef VTE_DEBUG
+#if VTE_DEBUG
   const GDebugKey keys[] = {
     { "misc",         VTE_DEBUG_MISC         },
     { "io",           VTE_DEBUG_IO           },
@@ -73,7 +73,7 @@ const char *
 _vte_debug_sequence_to_string(const char *str,
                               gssize length)
 {
-#if defined(VTE_DEBUG)
+#if VTE_DEBUG
         static const char codes[][6] = {
                 "NUL", "SOH", "STX", "ETX", "EOT", "ENQ", "ACK", "BEL",
                 "BS", "HT", "LF", "VT", "FF", "CR", "SO", "SI",
@@ -126,7 +126,7 @@ _vte_debug_sequence_to_string(const char *str,
 #endif /* VTE_DEBUG */
 }
 
-#ifdef VTE_DEBUG
+#if VTE_DEBUG
 static bool
 hexdump_line(GString* str,
              size_t ofs,
@@ -157,7 +157,7 @@ _vte_debug_hexdump(char const* str,
                    uint8_t const* buf,
                    size_t len)
 {
-#ifdef VTE_DEBUG
+#if VTE_DEBUG
         GString* s = g_string_new(str);
         g_string_append_printf(s, " len = 0x%x = %u\n", (unsigned int)len, (unsigned int)len);
 

src/debug.h
@@ -87,13 +87,13 @@ _vte_debug_on(guint flags)
 	return (_vte_debug_flags & flags) != 0;
 }
 
-#ifdef VTE_DEBUG
+#if VTE_DEBUG
 #define _VTE_DEBUG_IF(flags) if (G_UNLIKELY (_vte_debug_on (flags)))
 #else
 #define _VTE_DEBUG_IF(flags) if (0)
 #endif
 
-#ifdef VTE_DEBUG
+#if VTE_DEBUG
 #if defined(__GNUC__) && G_HAVE_GNUC_VARARGS
 #define _vte_debug_print(flags, fmt, ...) \
 	G_STMT_START { _VTE_DEBUG_IF(flags) g_printerr(fmt, ##__VA_ARGS__); } G_STMT_END

commit 5b198b4458b71629c02be154f18f09877dbb02fc

widget: Fix conditional
Fixes a regression from commit fb3220bee959d37f58daf702165f142ef9fab9f4.

src/vte.cc
@@ -7792,7 +7792,7 @@ Terminal::Terminal(vte::platform::Widget* w,
         }
 #endif
 
-#ifndef WITH_GNUTLS
+#if !WITH_GNUTLS
         std::string str{"\e[1m\e[31m"};
         str.append(_("WARNING"));
         str.append(":\e[39m ");