7fb5b2395c9e9c786c2277bc98d7e2661485fb10
Christian Persch <chpe@src.gnome.org>
2023-09-12 23:06:08 +0200

    widget: Fix conditional
    Fixes a regression from commit fb3220bee959d37f58daf702165f142ef9fab9f4.
    
    (cherry picked from commit 5b198b4458b71629c02be154f18f09877dbb02fc)

--- src/vte.cc
+++ src/vte.cc
@@ -7692,7 +7692,7 @@ Terminal::Terminal(vte::platform::Widget* w,
         }
 #endif
 
-#ifndef WITH_GNUTLS
+#if !WITH_GNUTLS
         std::string str{"\e[1m\e[31m"};
         str.append(_("WARNING"));
         str.append(":\e[39m ");
