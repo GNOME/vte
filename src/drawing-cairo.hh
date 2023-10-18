c99989d7d72019f3f5411cce9db97088728895c7
Christian Persch <chpe@src.gnome.org>
2023-10-18 22:11:24 +0200

    draw: Style fixes

--- src/drawing-cairo.hh
+++ src/drawing-cairo.hh
@@ -23,59 +23,63 @@
 namespace vte {
 namespace view {
 
-class DrawingCairo : public DrawingContext {
+class DrawingCairo final : public DrawingContext {
 public:
         DrawingCairo() noexcept = default;
-        ~DrawingCairo() noexcept = default;
+        ~DrawingCairo() noexcept override = default;
 
         DrawingCairo(DrawingCairo const&) = delete;
         DrawingCairo(DrawingCairo&&) = delete;
         DrawingCairo& operator=(DrawingCairo const&) = delete;
         DrawingCairo& operator=(DrawingCairo&&) = delete;
 
-        virtual cairo_t* begin_cairo(int x, int y, int width, int height) const;
-        virtual void end_cairo(cairo_t *cr) const;
+        cairo_t* begin_cairo(int x,
+                             int y,
+                             int width,
+                             int height) const override;
+        void end_cairo(cairo_t *cr) const override;
 
-        virtual void clip(Rectangle const* rect) const;
-        virtual void unclip() const;
+        void clip(Rectangle const* rect) const override;
+        void unclip() const override;
 
-        virtual void translate(double x, double y) const;
-        virtual void untranslate() const;
+        void translate(double x,
+                       double y) const override;
+        void untranslate() const override;
 
-        virtual void clear(int x,
-                           int y,
-                           int width,
-                           int height,
-                           vte::color::rgb const* color,
-                           double alpha) const;
-        virtual void fill_rectangle(int x,
-                                    int y,
-                                    int width,
-                                    int height,
-                                    vte::color::rgb const* color,
-                                    double alpha) const;
-        virtual void draw_rectangle(int x,
-                                    int y,
-                                    int width,
-                                    int height,
-                                    vte::color::rgb const* color,
-                                    double alpha) const;
+        void clear(int x,
+                   int y,
+                   int width,
+                   int height,
+                   vte::color::rgb const* color,
+                   double alpha) const override;
+        void fill_rectangle(int x,
+                            int y,
+                            int width,
+                            int height,
+                            vte::color::rgb const* color,
+                            double alpha) const override;
+        void draw_rectangle(int x,
+                            int y,
+                            int width,
+                            int height,
+                            vte::color::rgb const* color,
+                            double alpha) const override;
 
         void set_cairo(cairo_t* cr) noexcept;
 
-        virtual void draw_surface_with_color_mask(cairo_surface_t *surface,
-                                                  int x,
-                                                  int y,
-                                                  int width,
-                                                  int height,
-                                                  vte::color::rgb const* color) const;
+        void draw_surface_with_color_mask(cairo_surface_t *surface,
+                                          int x,
+                                          int y,
+                                          int width,
+                                          int height,
+                                          vte::color::rgb const* color) const override;
 
 protected:
-        virtual void draw_text_internal(TextRequest* requests,
-                                        gsize n_requests,
-                                        uint32_t attr,
-                                        vte::color::rgb const* color,
-                                        double alpha);
+        void draw_text_internal(TextRequest* requests,
+                                gsize n_requests,
+                                uint32_t attr,
+                                vte::color::rgb const* color,
+                                double alpha) override;
 
 private:
         cairo_t *m_cr{nullptr}; // unowned
--- src/drawing-gsk.hh
+++ src/drawing-gsk.hh
@@ -32,10 +32,10 @@
 namespace vte {
 namespace view {
 
-class DrawingGsk : public DrawingContext {
+class DrawingGsk final : public DrawingContext {
 public:
         DrawingGsk() noexcept;
-        virtual ~DrawingGsk();
+        ~DrawingGsk() override;
 
         DrawingGsk(DrawingGsk const&) = delete;
         DrawingGsk(DrawingGsk&&) = delete;
@@ -44,64 +44,72 @@ public:
 
         void set_snapshot(GtkSnapshot *snapshot) noexcept;
 
-        virtual cairo_t* begin_cairo(int x, int y, int width, int height) const;
-        virtual void end_cairo(cairo_t *cr) const;
+        cairo_t* begin_cairo(int x,
+                             int y,
+                             int width,
+                             int height) const override;
+        void end_cairo(cairo_t *cr) const override;
 
         /* We don't perform any clipping because we render the entire
          * scene graph and let Gsk perform the difference to determine
          * the appropriate damage area.
          */
-        inline virtual void clip(Rectangle const* rect) const {}
-        inline virtual void unclip() const {}
+        inline void clip(Rectangle const* rect) const override { }
+        inline void unclip() const override { }
 
-        inline virtual void clip_border(Rectangle const* rect) const {
+        inline void clip_border(Rectangle const* rect) const override
+        {
                 gtk_snapshot_push_clip(m_snapshot, rect->graphene());
         }
-        inline virtual void unclip_border() const {
+        inline void unclip_border() const override
+        {
                 gtk_snapshot_pop(m_snapshot);
         }
 
-        inline virtual void translate(double x, double y) const {
+        inline void translate(double x,
+                              double y) const override
+        {
                 auto const point = GRAPHENE_POINT_INIT((float)x, (float)y);
                 gtk_snapshot_save(m_snapshot);
                 gtk_snapshot_translate(m_snapshot, &point);
         }
-        inline virtual void untranslate() const {
+        inline void untranslate() const override
+        {
                 gtk_snapshot_restore(m_snapshot);
         }
 
-        virtual void clear(int x,
-                           int y,
-                           int width,
-                           int height,
-                           vte::color::rgb const* color,
-                           double alpha) const;
-        virtual void fill_rectangle(int x,
-                                    int y,
-                                    int width,
-                                    int height,
-                                    vte::color::rgb const* color,
-                                    double alpha) const;
-        virtual void draw_rectangle(int x,
-                                    int y,
-                                    int width,
-                                    int height,
-                                    vte::color::rgb const* color,
-                                    double alpha) const;
+        void clear(int x,
+                   int y,
+                   int width,
+                   int height,
+                   vte::color::rgb const* color,
+                   double alpha) const override;
+        void fill_rectangle(int x,
+                            int y,
+                            int width,
+                            int height,
+                            vte::color::rgb const* color,
+                            double alpha) const override;
+        void draw_rectangle(int x,
+                            int y,
+                            int width,
+                            int height,
+                            vte::color::rgb const* color,
+                            double alpha) const override;
 
-        virtual void draw_surface_with_color_mask(GdkTexture *texture,
-                                                  int x,
-                                                  int y,
-                                                  int width,
-                                                  int height,
-                                                  vte::color::rgb const* color) const;
+        void draw_surface_with_color_mask(GdkTexture *texture,
+                                          int x,
+                                          int y,
+                                          int width,
+                                          int height,
+                                          vte::color::rgb const* color) const override;
 
 protected:
-        virtual void draw_text_internal(TextRequest* requests,
-                                        gsize n_requests,
-                                        uint32_t attr,
-                                        vte::color::rgb const* color,
-                                        double alpha);
+        void draw_text_internal(TextRequest* requests,
+                                gsize n_requests,
+                                uint32_t attr,
+                                vte::color::rgb const* color,
+                                double alpha) override;
 
 private:
         GtkSnapshot *m_snapshot{nullptr}; // unowned
