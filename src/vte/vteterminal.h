4a68971caca57a4fcdc02a6833423ff466f3d65d
Christian Persch <chpe@src.gnome.org>
2024-07-02 18:28:28 +0200

    widget: termprops: Remove reset function
    Remove API to reset a termprop for now. Termprop values are set/reset
    from the terminal side, not the API.  They may be added back if a use
    case manifests.

--- src/vte/vteterminal.h
+++ src/vte/vteterminal.h
@@ -799,14 +799,6 @@ _VTE_PUBLIC
 GVariant* vte_terminal_ref_termprop_variant_by_id(VteTerminal* terminal,
                                                   int prop) _VTE_CXX_NOEXCEPT _VTE_GNUC_NONNULL(1);
 
-_VTE_PUBLIC
-void vte_terminal_reset_termprop(VteTerminal* terminal,
-                                 char const* prop) _VTE_CXX_NOEXCEPT _VTE_GNUC_NONNULL(1, 2);
-
-_VTE_PUBLIC
-void vte_terminal_reset_termprop_by_id(VteTerminal* terminal,
-                                       int prop) _VTE_CXX_NOEXCEPT _VTE_GNUC_NONNULL(1);
-
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(VteTerminal, g_object_unref)
 
 G_END_DECLS
