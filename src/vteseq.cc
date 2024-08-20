GNOME/vte

commit f44e1c207d49cf33a3efe57f5cc68948c18675c8

widget: termprops: Inhibit notification when there is no change
When setting the cwd or cwf termprops to their current value, or resetting
an already-reset termprop, don't notify.

src/vteseq.cc
@@ -1430,62 +1430,58 @@ Terminal::reset_color(vte::parser::Sequence const& seq,
 }
 
 void
-Terminal::set_current_directory_uri(vte::parser::Sequence const& seq,
-                                              vte::parser::StringTokeniser::const_iterator& token,
-                                              vte::parser::StringTokeniser::const_iterator const& endtoken) noexcept
+Terminal::set_termprop_uri(vte::parser::Sequence const& seq,
+                           vte::parser::StringTokeniser::const_iterator& token,
+                           vte::parser::StringTokeniser::const_iterator const& endtoken,
+                           int termprop_id,
+                           PendingChanges legacy_pending_change) noexcept
 {
-        auto const info = get_termprop_info(VTE_TERMPROP_CURRENT_DIRECTORY_URI);
+        auto const info = get_termprop_info(termprop_id);
         assert(info);
 
+        auto set = false;
         if (token != endtoken && token.size_remaining() > 0) {
-                if (auto uri = vte::take_freeable(g_uri_parse(token.string_remaining().c_str(),
-                                                              GUriFlags(G_URI_FLAGS_ENCODED),
-                                                              nullptr));
-                    uri &&
-                    g_strcmp0(g_uri_get_scheme(uri.get()), "file") == 0) {
+                auto const str = token.string_remaining();
 
-                        m_termprops_dirty.at(info->id()) = true;
-                        m_termprop_values.at(info->id()) = std::move(uri);
-                } else {
-                        // invalid URI, or not a file: URI
-                        reset_termprop(*info);
+                // Only parse the URI if the termprop doesn't already have the
+                // same string value
+                if (auto const old_value = get_termprop(*info);
+                    !old_value ||
+                    !std::holds_alternative<vte::terminal::TermpropURIValue>(*old_value) ||
+                    std::get<vte::terminal::TermpropURIValue>(*old_value).second != str) {
+
+                        if (auto uri = vte::take_freeable(g_uri_parse(str.c_str(),
+                                                                      GUriFlags(G_URI_FLAGS_ENCODED),
+                                                                      nullptr));
+                            uri &&
+                            g_strcmp0(g_uri_get_scheme(uri.get()), "file") == 0) {
+
+                                set = true;
+                                m_termprops_dirty.at(info->id()) = true;
+                                m_termprop_values.at(info->id()) =
+                                        vte::terminal::TermpropValue{std::in_place_type<vte::terminal::TermpropURIValue>,
+                                                                     std::move(uri),
+                                                                     str};
+                        } else {
+                                // invalid URI, or not a file: URI
+                                set = true;
+                                reset_termprop(*info);
+                        }
                 }
         } else {
-                reset_termprop(*info);
-        }
-
-        m_pending_changes |= vte::to_integral(PendingChanges::TERMPROPS) |
-                vte::to_integral(PendingChanges::CWD);
-}
-
-void
-Terminal::set_current_file_uri(vte::parser::Sequence const& seq,
-                                         vte::parser::StringTokeniser::const_iterator& token,
-                                         vte::parser::StringTokeniser::const_iterator const& endtoken) noexcept
-
-{
-        auto const info = get_termprop_info(VTE_TERMPROP_CURRENT_FILE_URI);
-        assert(info);
-
-        if (token != endtoken && token.size_remaining() > 0) {
-                if (auto uri = vte::take_freeable(g_uri_parse(token.string_remaining().c_str(),
-                                                              GUriFlags(G_URI_FLAGS_ENCODED),
-                                                              nullptr));
-                    uri &&
-                    g_strcmp0(g_uri_get_scheme(uri.get()), "file") == 0) {
-
-                        m_termprops_dirty.at(info->id()) = true;
-                        m_termprop_values.at(info->id()) = std::move(uri);
-                } else {
-                        // invalid URI, or not a file: URI
+                // Only reset the termprop if it's not already reset
+                if (auto const old_value = get_termprop(*info);
+                    !old_value ||
+                    !std::holds_alternative<std::monostate>(*old_value)) {
+                        set = true;
                         reset_termprop(*info);
                 }
-        } else {
-                reset_termprop(*info);
         }
 
-        m_pending_changes |= vte::to_integral(PendingChanges::TERMPROPS) |
-                vte::to_integral(PendingChanges::CWF);
+        if (set) {
+                m_pending_changes |= vte::to_integral(PendingChanges::TERMPROPS) |
+                        vte::to_integral(legacy_pending_change);
+        }
 }
 
 void
@@ -6847,11 +6843,15 @@ Terminal::OSC(vte::parser::Sequence const& seq)
 
         switch (osc) {
         case VTE_OSC_VTECWF:
-                set_current_file_uri(seq, it, cend);
+                set_termprop_uri(seq, it, cend,
+                                 VTE_PROPERTY_ID_CURRENT_FILE_URI,
+                                 PendingChanges::CWF);
                 break;
 
         case VTE_OSC_VTECWD:
-                set_current_directory_uri(seq, it, cend);
+                set_termprop_uri(seq, it, cend,
+                                 VTE_PROPERTY_ID_CURRENT_DIRECTORY_URI,
+                                 PendingChanges::CWF);
                 break;
 
         case VTE_OSC_VTEHYPER:

src/termprops-test.cc
@@ -151,11 +151,13 @@ assert_termprop_parse_uri(std::string_view const& str,
         auto const value = parse_termprop_value(TermpropType::URI, str);
         assert(value);
         assert(!value->valueless_by_exception());
-        assert(std::holds_alternative<vte::Freeable<GUri>>(*value));
+        assert(std::holds_alternative<vte::terminal::TermpropURIValue>(*value));
 
-        auto u = vte::glib::take_string(g_uri_to_string(std::get<vte::Freeable<GUri>>(*value).get()));
-        assert(u);
-        assert(str == u.get());
+        assert(str == std::get<vte::terminal::TermpropURIValue>(*value).second);
+
+        auto ustr = vte::glib::take_string(g_uri_to_string(std::get<vte::terminal::TermpropURIValue>(*value).first.get()));
+        assert(ustr);
+        assert(str == ustr.get());
 }
 
 static void

src/termprops.hh
@@ -30,6 +30,7 @@
 #include <string>
 #include <string_view>
 #include <unordered_map>
+#include <utility>
 #include <variant>
 #include <version>
 
@@ -253,6 +254,8 @@ using termprop_rgba = vte::color::rgba_base<float>;
 using termprop_rgba = vte::color::rgba_base<double>;
 #endif
 
+using TermpropURIValue = std::pair<vte::Freeable<GUri>, std::string>;
+
 using TermpropValue = std::variant<std::monostate,
                                    bool,
                                    int64_t,
@@ -261,7 +264,7 @@ using TermpropValue = std::variant<std::monostate,
                                    termprop_rgba,
                                    vte::uuid,
 				   std::string,
-                                   vte::Freeable<GUri>>;
+                                   TermpropURIValue>;
 
 namespace impl {
 
@@ -524,16 +527,16 @@ parse_termprop_uri(std::string_view str)
             uri &&
             g_uri_get_scheme(uri.get()) &&
             !g_str_equal(g_uri_get_scheme(uri.get()), "data")) {
-                return std::make_optional<TermpropValue>(std::move(uri));
+                return std::make_optional<TermpropValue>(std::in_place_type<TermpropURIValue>, std::move(uri), str);
         }
 
         return std::nullopt;
 }
 
 inline std::optional<std::string>
-unparse_termprop_uri(vte::Freeable<GUri> const& uri)
+unparse_termprop_uri(TermpropURIValue const& v)
 {
-        return std::make_optional<std::string>(vte::glib::take_string(g_uri_to_string(uri.get())).get());
+        return std::make_optional<std::string>(v.second);
 }
 
 } // namespace impl
@@ -641,8 +644,8 @@ unparse_termprop_value(TermpropType type,
                 break;
 
         case URI:
-                if (std::holds_alternative<vte::Freeable<GUri>>(value)) {
-                        return impl::unparse_termprop_uri(std::get<vte::Freeable<GUri>>(value));
+                if (std::holds_alternative<TermpropURIValue>(value)) {
+                        return impl::unparse_termprop_uri(std::get<TermpropURIValue>(value));
                 }
                 break;
 

src/vtegtk.cc
@@ -129,18 +129,6 @@ sanitise_widget_size_request(int* minimum,
         *natural = std::clamp(*natural, *minimum, limit);
 }
 
-#define VTE_CACHE_KEY_CWD (g_quark_from_static_string("Vte::Cache::CWD"))
-#define VTE_CACHE_KEY_CWF (g_quark_from_static_string("Vte::Cache::CWF"))
-
-static char const*
-cache_string_data(VteTerminal* terminal,
-                  GQuark key,
-                  char* str) noexcept
-{
-        g_object_set_qdata_full(G_OBJECT(terminal), key, str, GDestroyNotify(g_free));
-        return str;
-}
-
 struct _VteTerminalClassPrivate {
         GtkStyleProvider *style_provider;
 };
@@ -6324,12 +6312,11 @@ try
 
         if (auto const value = widget->get_termprop(*info);
             value &&
-            std::holds_alternative<vte::Freeable<GUri>>(*value)) {
-                return cache_string_data(terminal, VTE_CACHE_KEY_CWD,
-                                         g_uri_to_string(std::get<vte::Freeable<GUri>>(*value).get()));
+            std::holds_alternative<vte::terminal::TermpropURIValue>(*value)) {
+                return std::get<vte::terminal::TermpropURIValue>(*value).second.c_str();
         }
 
-        return cache_string_data(terminal, VTE_CACHE_KEY_CWD, nullptr);
+        return nullptr;
 }
 catch (...)
 {
@@ -6359,12 +6346,11 @@ try
 
         if (auto const value = widget->get_termprop(*info);
             value &&
-            std::holds_alternative<vte::Freeable<GUri>>(*value)) {
-                return cache_string_data(terminal, VTE_CACHE_KEY_CWF,
-                                         g_uri_to_string(std::get<vte::Freeable<GUri>>(*value).get()));
+            std::holds_alternative<vte::terminal::TermpropURIValue>(*value)) {
+                return std::get<vte::terminal::TermpropURIValue>(*value).second.c_str();
         }
 
-        return cache_string_data(terminal, VTE_CACHE_KEY_CWF, nullptr);
+        return nullptr;
 }
 catch (...)
 {
@@ -9060,8 +9046,8 @@ try
 
         auto const value = widget->get_termprop(*info);
         if (value &&
-            std::holds_alternative<vte::Freeable<GUri>>(*value)) {
-                return g_uri_ref(std::get<vte::Freeable<GUri>>(*value).get());
+            std::holds_alternative<vte::terminal::TermpropURIValue>(*value)) {
+                return g_uri_ref(std::get<vte::terminal::TermpropURIValue>(*value).first.get());
         }
 
         return nullptr;
@@ -9202,10 +9188,10 @@ try
                 break;
 
         case vte::terminal::TermpropType::URI:
-                if (std::holds_alternative<vte::Freeable<GUri>>(*value)) {
+                if (std::holds_alternative<vte::terminal::TermpropURIValue>(*value)) {
                         rv = true;
                         g_value_init(gvalue, G_TYPE_URI);
-                        g_value_set_boxed(gvalue, std::get<vte::Freeable<GUri>>(*value).get());
+                        g_value_set_boxed(gvalue, std::get<vte::terminal::TermpropURIValue>(*value).first.get());
                 }
                 break;
 
@@ -9353,8 +9339,8 @@ try
                 break;
 
         case vte::terminal::TermpropType::URI:
-                if (std::holds_alternative<vte::Freeable<GUri>>(*value)) {
-                        return g_variant_new_string(vte::glib::take_string(g_uri_to_string(std::get<vte::Freeable<GUri>>(*value).get())).get());
+                if (std::holds_alternative<vte::terminal::TermpropURIValue>(*value)) {
+                        return g_variant_new_string(std::get<vte::terminal::TermpropURIValue>(*value).second.c_str());
                 }
                 break;
 

src/vteinternal.hh
@@ -1782,12 +1782,11 @@ public:
                          vte::parser::StringTokeniser::const_iterator& token,
                          vte::parser::StringTokeniser::const_iterator const& endtoken,
                          osc_colors::OSCValuedColorSequenceKind osc_kind) noexcept;
-        void set_current_directory_uri(vte::parser::Sequence const& seq,
-                                       vte::parser::StringTokeniser::const_iterator& token,
-                                       vte::parser::StringTokeniser::const_iterator const& endtoken) noexcept;
-        void set_current_file_uri(vte::parser::Sequence const& seq,
-                                  vte::parser::StringTokeniser::const_iterator& token,
-                                  vte::parser::StringTokeniser::const_iterator const& endtoken) noexcept;
+        void set_termprop_uri(vte::parser::Sequence const& seq,
+                              vte::parser::StringTokeniser::const_iterator& token,
+                              vte::parser::StringTokeniser::const_iterator const& endtoken,
+                              int termprop_id,
+                              PendingChanges legacy_pending_change) noexcept;
         void set_current_hyperlink(vte::parser::Sequence const& seq,
                                    vte::parser::StringTokeniser::const_iterator& token,
                                    vte::parser::StringTokeniser::const_iterator const& endtoken) noexcept;

commit 9e70343560b1916a83d5a4d7323a3b8b48d82f12

widget: Fix legacy OSC 7 notification
Commit f44e1c207d49cf33a3efe57f5cc68948c18675c8 accidentally made OSC 7
emit the legacy OSC 6 notification.

Fixes: https://gitlab.gnome.org/GNOME/vte/-/issues/2806

src/vteseq.cc
@@ -7594,7 +7594,7 @@ Terminal::OSC(vte::parser::Sequence const& seq)
         case VTE_OSC_VTECWD:
                 set_termprop_uri(seq, it, cend,
                                  VTE_PROPERTY_ID_CURRENT_DIRECTORY_URI,
-                                 PendingChanges::CWF);
+                                 PendingChanges::CWD);
                 break;
 
         case VTE_OSC_VTEHYPER: