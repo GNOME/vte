GNOME/vte

commit 667a2abf89d9247f9d537b33dd501416e54ab951

app: Set desktop file name as xproperty
This seems to fix issue 2849 on x11. Still pending a fix for wayland.

https://gitlab.gnome.org/GNOME/vte/-/issues/2849

src/app/app.cc
@@ -3959,6 +3959,22 @@ vteapp_window_realize(GtkWidget* widget)
                          G_CALLBACK(window_toplevel_notify_state_cb), window);
 #endif
 
+#ifdef GDK_WINDOWING_X11
+#if VTE_GTK == 3
+        if (GDK_IS_X11_WINDOW(win)) {
+                gdk_x11_window_set_utf8_property(win,
+                                                 "_KDE_NET_WM_DESKTOP_FILE",
+                                                 "vte-gtk3");
+        }
+#elif VTE_GTK == 4
+        if (GDK_IS_X11_SURFACE(surface)) {
+                gdk_x11_surface_set_utf8_property(surface,
+                                                  "_KDE_NET_WM_DESKTOP_FILE",
+                                                  "vte-gtk4");
+        }
+#endif // VTE_GTK
+#endif // GDK_WINDOWING_X11
+
         window_update_fullscreen_state(window);
 
         vteapp_window_resize(window);

src/app/vte.desktop.in
@@ -8,7 +8,7 @@ Icon=org.gnome.Terminal
 Type=Application
 Categories=GNOME;GTK;System;TerminalEmulator;Utility;X-GNOME-Utilities;
 StartupNotify=true
-StartupWMClass=Vte
+StartupWMClass=Terminal
 SingleMainWindow=false
 X-ExecArg=--
 X-KDE-AuthorizeAction=shell_access

commit ed8949efc22863e7730e998638713fae838f8fea

build: Add missing gdkx includes
Fixes a build problem introduced in commit
667a2abf89d9247f9d537b33dd501416e54ab951.

Fixes: https://gitlab.gnome.org/GNOME/vte/-/issues/2851

src/app/app.cc
@@ -34,6 +34,14 @@
 #include <cairo/cairo-gobject.h>
 #include <vte/vte.h>
 
+#ifdef GDK_WINDOWING_X11
+#if VTE_GTK == 3
+#include <gdk/gdkx.h>
+#elif VTE_GTK == 4
+#include <gdk/x11/gdkx.h>
+#endif
+#endif
+
 #include <algorithm>
 #include <cmath>
 #include <vector>